76d7a12dbe23d1051a2d133d71e5a618af558c49
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-05-06 16:03:49 +0200

    cleanings of python code, pep8, lint, various improvements / simplification

--- analytic_multicurrency/analytic.py
+++ analytic_multicurrency/analytic.py
@@ -19,32 +19,33 @@
 #
 ##############################################################################
 
+from itertools import product
 from openerp.osv import orm, fields
 import openerp.addons.decimal_precision as dp
+
 
 class account_analytic_account(orm.Model):
     _inherit = 'account.analytic.account'
 
     def _debit_credit_bal_qtty(self, cr, uid, ids, name, arg, context=None):
         """Replace the original amount column by aa_amount_currency"""
-        res = {}
         if context is None:
             context = {}
-        child_ids = tuple(self.search(cr, uid, [('parent_id', 'child_of', ids)]))
-        for i in child_ids:
-            res[i] =  {}
-            for n in name:
-                res[i][n] = 0.0
-
+        child_ids = self.search(cr, uid,
+                                [('parent_id', 'child_of', ids)],
+                                context=context)
+        sums = {}
         if not child_ids:
-            return res
+            return sums
+        for child_id, field in product(child_ids, name):
+            sums.setdefault(child_id, {})[name] = 0.0
 
         where_date = ''
         where_clause_args = [tuple(child_ids)]
-        if context.get('from_date', False):
+        if context.get('from_date'):
             where_date += " AND l.date >= %s"
-            where_clause_args  += [context['from_date']]
-        if context.get('to_date', False):
+            where_clause_args += [context['from_date']]
+        if context.get('to_date'):
             where_date += " AND l.date <= %s"
             where_clause_args += [context['to_date']]
         cr.execute("""
@@ -69,23 +70,53 @@ class account_analytic_account(orm.Model):
               """ + where_date + """
               GROUP BY a.id""", where_clause_args)
         for ac_id, debit, credit, balance, quantity in cr.fetchall():
-            res[ac_id] = {'debit': debit, 'credit': credit, 'balance': balance, 'quantity': quantity}
-        return self._compute_level_tree(cr, uid, ids, child_ids, res, ['debit', 'credit', 'balance', 'quantity'], context)
+            sums[ac_id] = {'debit': debit,
+                           'credit': credit,
+                           'balance': balance,
+                           'quantity': quantity}
+        return self._compute_level_tree(cr, uid, ids, child_ids, sums,
+                                        ['debit', 'credit',
+                                         'balance', 'quantity'],
+                                        context)
 
     _columns = {
-        'balance': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Balance', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
-        'debit': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Debit', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
-        'credit': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Credit', multi='debit_credit_bal_qtty', digits_compute=dp.get_precision('Account')),
-        'quantity': fields.function(_debit_credit_bal_qtty, method=True, type='float', string='Quantity', multi='debit_credit_bal_qtty'),
-   }
+        'balance': fields.function(_debit_credit_bal_qtty,
+                                   type='float',
+                                   string='Balance',
+                                   multi='debit_credit_bal_qtty',
+                                   digits_compute=dp.get_precision('Account')),
+        'debit': fields.function(_debit_credit_bal_qtty,
+                                 type='float',
+                                 string='Debit',
+                                 multi='debit_credit_bal_qtty',
+                                 digits_compute=dp.get_precision('Account')),
+        'credit': fields.function(_debit_credit_bal_qtty,
+                                  type='float',
+                                  string='Credit',
+                                  multi='debit_credit_bal_qtty',
+                                  digits_compute=dp.get_precision('Account')),
+        'quantity': fields.function(_debit_credit_bal_qtty,
+                                    type='float',
+                                    string='Quantity',
+                                    multi='debit_credit_bal_qtty'),
+    }
 
-    # We remove the currency constraint cause we want to let the user choose another
-    # currency than the company one. Don't be able to override properly this constraints :(
+    # We remove the currency constraint cause we want to let the user
+    # choose another currency than the company one. Don't be able to
+    # override properly this constraints :(
     def check_currency(self, cr, uid, ids, context=None):
         return True
+
     def check_recursion(self, cr, uid, ids, parent=None):
-        return super(account_analytic_account, self)._check_recursion(cr, uid, ids, parent=parent)
+        return super(account_analytic_account, self)._check_recursion(
+            cr, uid, ids, parent=parent)
+
     _constraints = [
-        (check_recursion, 'Error! You can not create recursive analytic accounts.', ['parent_id']),
-        (check_currency, 'Error! The currency has to be the same as the currency of the selected company', ['currency_id', 'company_id']),
+        (check_recursion,
+         'Error. You can not create recursive analytic accounts.',
+         ['parent_id']),
+        (check_currency,
+         'Error. The currency has to be the same as the currency of '
+         'the selected company.',
+         ['currency_id', 'company_id']),
     ]
--- analytic_multicurrency/analytic_analysis.py
+++ analytic_multicurrency/analytic_analysis.py
@@ -19,6 +19,7 @@
 #
 ##############################################################################
 
+from itertools import product
 from openerp.osv import orm, fields
 import openerp.addons.decimal_precision as dp
 
@@ -28,64 +29,79 @@ class account_analytic_account(orm.Model):
 
     def _ca_invoiced_calc(self, cr, uid, ids, name, arg, context=None):
         """Replace the original amount column by aa_amount_currency"""
-        res = {}
-        res_final = {}
-        child_ids = tuple(self.search(cr, uid, [('parent_id', 'child_of', ids)], context=context))
-        for i in child_ids:
-            res[i] =  {}
-            for n in [name]:
-                res[i][n] = 0.0
+        child_ids = self.search(cr, uid,
+                                [('parent_id', 'child_of', ids)],
+                                context=context)
+        sums = {}
         if not child_ids:
             return res
+        for child_id, field in product(child_ids, name):
+            sums.setdefault(child_id, {})[name] = 0.0
 
-        if child_ids:
-            cr.execute("SELECT account_analytic_line.account_id, COALESCE(SUM(aa_amount_currency), 0.0) \
-                    FROM account_analytic_line \
-                    JOIN account_analytic_journal \
-                        ON account_analytic_line.journal_id = account_analytic_journal.id  \
-                    WHERE account_analytic_line.account_id IN %s \
-                        AND account_analytic_journal.type = 'sale' \
-                    GROUP BY account_analytic_line.account_id", (child_ids,))
-            for account_id, sum in cr.fetchall():
-                res[account_id][name] = round(sum,2)
-        data = self._compute_level_tree(cr, uid, ids, child_ids, res, [name], context=context)
-        for i in data:
-            res_final[i] = data[i][name]
-        return res_final
+        sql = ("SELECT account_analytic_line.account_id, "
+               "       COALESCE(SUM(aa_amount_currency), 0.0) "
+               "FROM account_analytic_line "
+               "JOIN account_analytic_journal "
+               "ON account_analytic_line.journal_id = account_analytic_journal.id "
+               "WHERE account_analytic_line.account_id IN %s "
+               "AND account_analytic_journal.type = 'sale' "
+               "GROUP BY account_analytic_line.account_id")
+        cr.execute(sql, (tuple(child_ids),))
+
+        precision_obj = self.pool.get('decimal.precision')
+        precision = precision_obj.precision_get(cr, uid, 'Account')
+        for account_id, row_sum in cr.fetchall():
+            sums[account_id][name] = round(row_sum, precision)
+        data = self._compute_level_tree(cr, uid, ids, child_ids,
+                                        sums, [name], context=context)
+        return dict((account_id, row[name]) for account_id, row
+                    in data.iteritems())
 
     def _total_cost_calc(self, cr, uid, ids, name, arg, context=None):
         """Replace the original amount column by aa_amount_currency"""
-        res = {}
-        res_final = {}
-        child_ids = tuple(self.search(cr, uid, [('parent_id', 'child_of', ids)], context=context))
-
-        for i in child_ids:
-            res[i] =  {}
-            for n in [name]:
-                res[i][n] = 0.0
+        child_ids = self.search(cr, uid,
+                                [('parent_id', 'child_of', ids)],
+                                context=context)
+        sums = {}
         if not child_ids:
-            return res
+            return sums
+        for child_id, field in product(child_ids, name):
+            sums.setdefault(child_id, {})[name] = 0.0
 
-        if child_ids:
-            cr.execute("""SELECT account_analytic_line.account_id, COALESCE(SUM(aa_amount_currency), 0.0) \
-                    FROM account_analytic_line \
-                    JOIN account_analytic_journal \
-                        ON account_analytic_line.journal_id = account_analytic_journal.id \
-                    WHERE account_analytic_line.account_id IN %s \
-                        AND amount<0 \
-                    GROUP BY account_analytic_line.account_id""",(child_ids,))
-            for account_id, sum in cr.fetchall():
-                res[account_id][name] = round(sum,2)
-        data = self._compute_level_tree(cr, uid, ids, child_ids, res, [name], context)
-        for i in data:
-            res_final[i] = data[i][name]
-        return res_final
+        sql = ("SELECT account_analytic_line.account_id, "
+               "       COALESCE(SUM(aa_amount_currency), 0.0) "
+               "FROM account_analytic_line "
+               "JOIN account_analytic_journal "
+               "ON account_analytic_line.journal_id = account_analytic_journal.id "
+               "WHERE account_analytic_line.account_id IN %s "
+               "AND amount < 0 "
+               "GROUP BY account_analytic_line.account_id")
+        cr.execute(sql, (tuple(child_ids),))
 
-    _columns ={
-    'ca_invoiced': fields.function(_ca_invoiced_calc, method=True, type='float', string='Invoiced Amount',
-        help="Total customer invoiced amount for this account.",
-        digits_compute=dp.get_precision('Account')),
-    'total_cost': fields.function(_total_cost_calc, method=True, type='float', string='Total Costs',
-        help="Total of costs for this account. It includes real costs (from invoices) and indirect costs, like time spent on timesheets.",
-        digits_compute=dp.get_precision('Account')),
-     }
+        precision_obj = self.pool.get('decimal.precision')
+        precision = precision_obj.precision_get(cr, uid, 'Account')
+        for account_id, row_sum in cr.fetchall():
+            sums[account_id][name] = round(row_sum, precision)
+        data = self._compute_level_tree(cr, uid, ids, child_ids,
+                                        sums, [name], context)
+        return dict((account_id, row[name]) for account_id, row
+                    in data.iteritems())
+
+    _columns = {
+        'ca_invoiced': fields.function(
+            _ca_invoiced_calc,
+            type='float',
+            string='Invoiced Amount',
+            help="Total customer invoiced amount for "
+                 "this account.",
+            digits_compute=dp.get_precision('Account')),
+        'total_cost': fields.function(
+            _total_cost_calc,
+            type='float',
+            string='Total Costs',
+            help="Total of costs for this account. "
+                 "It includes real costs (from "
+                 "invoices) and indirect costs, "
+                 "like time spent on timesheets.",
+            digits_compute=dp.get_precision('Account')),
+    }
