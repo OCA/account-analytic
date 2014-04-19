92bfe98b950d4b24ddf215fdcc4153634502e2dc
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-04-19 12:20:24 +0200

    [IMP] comments in test suite + long lines __openerp__.py

--- account_analytic_required/__openerp__.py
+++ account_analytic_required/__openerp__.py
@@ -30,11 +30,16 @@
 Account Analytic Required
 =========================
 
-This module adds an option *analytic policy* on account types. You have the choice between 3 policies : *always*, *never* and *optional*.
+This module adds an option *analytic policy* on account types.
+You have the choice between 3 policies : *always*, *never* and *optional*.
 
-For example, if you want to have an analytic account on all your expenses, set the policy to *always* for the account type *expense* ; then, if you try to save an account move line with an account of type *expense* without analytic account, you will get an error message.
+For example, if you want to have an analytic account on all your expenses,
+set the policy to *always* for the account type *expense* ; then, if you
+try to save an account move line with an account of type *expense*
+without analytic account, you will get an error message.
 
-Module developped by Alexis de Lattre <alexis.delattre@akretion.com> during the Akretion-Camptocamp code sprint of June 2011.
+Module developped by Alexis de Lattre <alexis.delattre@akretion.com>
+during the Akretion-Camptocamp code sprint of June 2011.
 """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com/',
