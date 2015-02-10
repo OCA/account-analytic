f5d696c013e01a4ef74fd8907d79544e400c44d6
Guillaume Auger <guillaume.auger@savoirfairelinux.com>
2015-02-10 15:22:16 -0500

    [ADD]account_analytic_unique_reference

---
+++ account_analytic_unique_reference/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 Savoir-faire Linux (<www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    "name": "Account Analytic Unique Reference",
+    "version": "0.1",
+    "author": 'Savoir-faire Linux',
+    "summary": "Add unique contraint on accout.analytic.account reference",
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    "category": 'Account Analytic',
+    "images": [],
+    "depends": ["base", "analytic"],
+    "data": [],
+    "test": [],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
