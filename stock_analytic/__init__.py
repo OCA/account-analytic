72866e6d64737c6704898b8ca460e329aa0c7a7f
fv-clearcorp <fabio.vilchez@clearcorp.co.cr>
2015-11-19 14:39:43 -0600

    [ADD] - Added new module stock_analytic
    This module allows the user to generate analytic information from stock
    moves.

--- stock_analytic/__init__.py
+++ stock_analytic/__init__.py
@@ -17,10 +17,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#################################################################################
-
+###############################################################################
 import stock
 import sale
 import purchase
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
