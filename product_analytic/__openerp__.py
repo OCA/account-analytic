3e996d39f34633247cff38b635ca6784c520425c
Alexis de Lattre <alexis.delattre@akretion.com>
2015-11-30 10:20:25 +0100

    [ADD] product_analytic

---
+++ product_analytic/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2015 Akretion (http://www.akretion.com/) - Alexis de Lattre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product Analytic',
+    'version': '8.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Add analytic account on products and product categories',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['account'],
+    'data': ['product_view.xml'],
+    'installable': True,
+}
