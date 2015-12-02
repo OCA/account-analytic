18bd4a5f247a1719c11a07e8f2d4907f2806b195
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-12-02 21:26:41 +0100

    [IMP] mrp_analytic: Tests + README + view + ìcon

--- mrp_analytic/__openerp__.py
+++ mrp_analytic/__openerp__.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Eficent - Jordi Ballester Alomar
+# © 2015 Pedro M. Baeza - Antiun Ingeniería
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'mrp_production_order_analytic',
+    'name': 'Analytic for manufacturing',
     'summary': 'Adds the analytic account to the production order',
     'version': '8.0.1.0.0',
     'category': "Manufacturing",
     'author': 'Eficent, '
+              'Antiun Ingeniería, '
+              'Serv. Tecnol. Avanzados - Pedro M. Baeza, '
               'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-analytic',
     'license': 'AGPL-3',
     'depends': ['mrp', 'analytic'],
     'data': [
