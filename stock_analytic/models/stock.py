87eccba3dabd99e44ed71d2391698ab32066e1b1
Andhitia Rama <andhitia.r@gmail.com>
2016-02-05 14:32:46 +0700

    Adapt original module according OCA Standard
    Remove sale and purchase dependency. Add test
    
    Only assign analytic account if account != valuation acc
    
    Changing field name account_analytic_id
    
    Adjust to OCA latest guidelines. Add some usahe info on README

---
+++ stock_analytic/models/stock.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Julius Network Solutions
+# Copyright 2015 Clear Corp
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models, api
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    analytic_account_id = fields.Many2one(
+        string='Analytic Account',
+        comodel_name='account.analytic.account',
+    )
+
+
+class StockQuant(models.Model):
+
+    _inherit = "stock.quant"
+
+    @api.model
+    def _prepare_account_move_line(self, move, qty, cost,
+                                   credit_account_id, debit_account_id,
+                                   context=None):
+        res = super(StockQuant,
+                    self)._prepare_account_move_line(
+                        move, qty, cost,
+                        credit_account_id,
+                        debit_account_id,
+                        context=context
+        )
+
+        # Add analytic account in debit line
+        if not move.analytic_account_id:
+            return res
+
+        for num in range(0, 2):
+            if res[num][2]["account_id"] != move.product_id.\
+                    categ_id.property_stock_valuation_account_id.id:
+                res[num][2].update({
+                    'analytic_account_id': move.analytic_account_id.id,
+                })
+        return res
