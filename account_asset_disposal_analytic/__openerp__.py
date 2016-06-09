c25d3832d855ab40bea135ffe41022382f2c4225
Antonio Espinosa <antespi@gmail.com>
2016-06-09 12:24:53 +0200

    [ADD] account_asset_disposal_analytic addon

---
+++ account_asset_disposal_analytic/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account asset disposal analytic",
+    "summary": "Sets asset analytic account to disposal move",
+    "version": "8.0.1.0.0",
+    "category": "Analytic Accounting",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "account_asset_disposal",
+        "account_asset_analytic",
+    ],
+}
