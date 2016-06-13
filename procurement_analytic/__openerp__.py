1e4d927f82efffa7b0e38f579a5849e6c6ef5550
Carlos Incaser <carlos@incaser.es>
2016-06-13 14:05:57 +0200

    procurement_analytic: New module

---
+++ procurement_analytic/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Procurement Analytic',
+    'summary': 'This module adds analytic account to procurements',
+    'version': '8.0.1.0.0',
+    'category': 'Analytic',
+    'license': 'AGPL-3',
+    'author': "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.tecnativa.com',
+    'depends': ['procurement'],
+    'data': [
+        'views/procurement_analytic.xml',
+    ],
+    'installable': True,
+}
