f91d9c1aaeb1acbd78d4c10f425c8ec283cd06db
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-27 10:10:24 +0200

    Migrate product_analytic to v9

---
+++ product_analytic/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015-2016 Akretion (http://www.akretion.com/) - Alexis de Lattre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product Analytic',
+    'version': '9.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Add analytic account on products and product categories',
+    'author': 'Akretion, '
+              'Antiun Ingeniería S.L., '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['account'],
+    'data': ['views/product.xml'],
+    'installable': True,
+}
