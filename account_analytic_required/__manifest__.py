336de9a36ede020d70c18f2d9711408310ae4163
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-10 20:52:41 +0200

    Port account_analytic_required to v10

--- account_analytic_required/__manifest__.py
+++ account_analytic_required/__manifest__.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2011 Akretion
+# © 2011-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     'name': 'Account Analytic Required',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Analytic Accounting',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com/',
     'depends': ['account'],
     'data': ['views/account.xml'],
-    'installable': False,
-    'auto_install': False,
+    'installable': True,
 }
