e02d63c025bb43624b38ea82de7fe9d47262a4f7
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-11 14:54:44 +0200

    Migration of account_analytic_no_lines to 10.0

--- account_analytic_no_lines/tests/test_account_analytic_no_lines.py
+++ account_analytic_no_lines/tests/test_account_analytic_no_lines.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import SavepointCase
+from odoo.tests.common import SavepointCase
 
 
 class TestAccountAnalyticNoLines(SavepointCase):
