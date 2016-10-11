0847395cd583e364ee6d7fa70f72b9d045884a2d
Thomas Binsfeld (ACSONE) <thomas.binsfeld@acsone.eu>
2016-10-11 10:15:13 +0200

    [9.0][ADD] account_analytic_no_lines (#69)
    [ADD] account_analytic_no_lines

---
+++ account_analytic_no_lines/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Analytic No Lines',
+    'summary': """
+        This module hides analytics lines from accounting menus and disable
+         their generation from an invoice or a move line.""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'www.acsone.eu',
+    'depends': [
+        'account',
+        'analytic',
+    ],
+    'data': [
+        'views/account_analytic_account.xml',
+        'data/data_account_analytic_group.xml',
+        'views/account_analytic.xml',
+    ],
+}
