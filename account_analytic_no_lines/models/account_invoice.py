18db9129ec3891996fa30dd94a30df04d04222d8
Thomas Binsfeld (ACSONE) <thomas.binsfeld@acsone.eu>
2016-10-11 10:15:13 +0200

    [9.0][ADD] account_analytic_no_lines (#69)
    [ADD] account_analytic_no_lines

---
+++ account_analytic_no_lines/models/account_invoice.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def finalize_invoice_move_lines(self, move_lines):
+        move_lines = super(AccountInvoice, self)\
+            .finalize_invoice_move_lines(move_lines)
+        move_lines[0][2].pop('analytic_line_ids')
+        return move_lines
