05f32a724530a44004f811eb1b6691e176d51c11
Andhitia Rama <andhitia.r@gmail.com>
2016-12-06 12:04:32 +0700

    Adjust to OCA latest guidelines. Add some usahe info on README

--- stock_analytic/tests/test_stock_picking.py
+++ stock_analytic/tests/test_stock_picking.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2013 Julius Network Solutions
-# © 2015 Clear Corp
-# © 2016 Andhitia Rama <andhitia.r@gmail.com>
+# Copyright 2013 Julius Network Solutions
+# Copyright 2015 Clear Corp
+# Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
