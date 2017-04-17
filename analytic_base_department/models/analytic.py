5862c889c559a7011cbb1b13e66cbdd0834c80a2
Dave Burkholder <dave@compassfoundation.io>
2017-04-17 17:13:34 -0400

    10.0 mig analytic_base_department

--- analytic_base_department/models/analytic.py
+++ analytic_base_department/models/analytic.py
@@ -2,7 +2,7 @@
 # © 2011-2016 Camptocamp SA
 # © 2013 Daniel Reis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class AnalyticAccount(models.Model):
