c4c15cfcc11fe77574a2ad83ebdbf1b600a9f3f8
Angel Moya Pardo <amoyapardo@gmail.com>
2017-04-21 09:35:49 +0200

    IMP analytic dimension abstract model & analytic dimension on invoice

--- analytic_tag_dimension/README.rst
+++ analytic_tag_dimension/README.rst
@@ -8,31 +8,28 @@ Analytic Tag Dimension
 
 This module allows to group Analytic Tags on Dimensions.
 
-Dimensions are created as custom field, then you can group by Dimensions on
-Analytic Entries Report.
+Dimensions are created as custom field, then you can group by Dimensions on Analytic Entries Report.
 
-When you set Tags on Analytic Entries each custom fields for dimensions are
-updated with its Tag. One Tag is only allowed on one Dimension, and you can not
-set more than one Tag from same Dimensions on Analytic Entry.
+When you set Tags on Analytic Entries each custom fields for dimensions are updated with its Tag.
+
+One Tag is only allowed on one Dimension, and you can not set more than one Tag from same Dimensions on Analytic Entry.
 
 
 Configuration
 =============
 
 To configure this module, you need to:
-* go to /Accounting/Configuration/Analytic Accounting/Analytic Accounts
-  Dimensions to create new Analytic Dimensions.
-
-* You can create new Analytic Tags on this form, or go to
-  /Accounting/Configuration/Analytic Accounting/Analytic Accounts Tags and set
-  Dimension for each Tag.
+* go to /Accounting/Configuration/Analytic Accounting/Analytic Accounts Dimensions to create new Analytic Dimensions.
+* You can create new Analytic Tags on this form, or go to /Accounting/Configuration/Analytic Accounting/Analytic Accounts Tags and set Dimension for each Tag.
 
 
 Known issues / Roadmap
 ======================
 
-* Analytic Entries with Tags created before install this module are not updated
-  with its Dimensions.
+* Analytic Entries with Tags created before install this module are not updated with its Dimensions.
+* Set color on Analytic Dimensions, and get it on Analytic Tags.
+* Change implementation to create stored computed fields, instead of rewrite create and write functions.
+* On function that create fields, get al models that inherit from AbstractModel
 
 Usage
 =====
--- analytic_tag_dimension/tests/test_analytic_dimension.py
+++ analytic_tag_dimension/tests/test_analytic_dimension.py
@@ -15,7 +15,6 @@ class TestAnalyticDimensionCase(TransactionCase):
         self.dimension_obj = self.env['account.analytic.dimension']
         self.tag_obj = self.env['account.analytic.tag']
         self.analytic_line_obj = self.env['account.analytic.line']
-        self.move_line_obj = self.env['account.move.line']
         self.account_obj = self.env['account.account']
 
         self.analytic_dimension_type = self.env.ref(
@@ -31,48 +30,33 @@ class TestAnalyticDimensionCase(TransactionCase):
         self.analytic_tag_concept_b = self.env.ref(
             'analytic_tag_dimension.analytic_tag_concept_b')
 
-        self.analytic_dimension_test = self.dimension_obj.create({
-            'name': 'Test_creation',
-            'code': 'test'
-        })
-        self.analytic_tag_test_a = self.tag_obj.create({
-            'name': 'Test A',
-            'analytic_dimension_id': self.analytic_dimension_test.id
-        })
-
         self.analytic_account_id = self.env.ref(
             'analytic.analytic_absences').id
 
-        account_user_type = self.env.ref(
+        self.account_user_type = self.env.ref(
             'account.data_account_type_receivable')
-        self.account = self.account_obj.create({
-            'code': 'test_acc',
-            'name': 'test account',
-            'user_type_id': account_user_type.id,
-            'reconcile': True,
-        })
-        journal = self.env['account.journal'].search([], limit=1)
+        self.journal = self.env['account.journal'].search([], limit=1)
 
-        company_id = self.env['res.users'].browse(self.env.uid).company_id.id
-
-        # create move
-        self.move = self.env['account.move'].create({
-            'name': '/',
-            'ref': '2011010',
-            'journal_id': journal.id,
-            'state': 'draft',
-            'company_id': company_id,
-        })
+        self.company_id = self.env['res.users'].browse(
+            self.env.uid).company_id.id
 
     def test_analytic_entry_dimension(self):
         """Test dimension creation on analytic entry creation
         """
+        analytic_dimension_test = self.dimension_obj.create({
+            'name': 'Test_creation',
+            'code': 'test'
+        })
+        analytic_tag_test_a = self.tag_obj.create({
+            'name': 'Test A',
+            'analytic_dimension_id': analytic_dimension_test.id
+        })
         values = {
             'account_id': self.analytic_account_id,
             'name': 'test',
             'tag_ids': [(6, 0, [self.analytic_tag_type_a.id,
                                 self.analytic_tag_concept_a.id,
-                                self.analytic_tag_test_a.id])]
+                                analytic_tag_test_a.id])]
         }
         line = self.analytic_line_obj.create(values)
         self.assertTrue(
@@ -89,20 +73,75 @@ class TestAnalyticDimensionCase(TransactionCase):
     def test_account_entry_dimension(self):
         """Test dimension creation on account move line creation
         """
+
+        account = self.account_obj.create({
+            'code': 'test_dimension_acc_01',
+            'name': 'test dimension account',
+            'user_type_id': self.account_user_type.id,
+            'reconcile': True,
+        })
+        self.move = self.env['account.move'].create({
+            'name': '/',
+            'ref': '2011010',
+            'journal_id': self.journal.id,
+            'state': 'draft',
+            'company_id': self.company_id,
+        })
         values = {
             'name': 'test',
-            'account_id': self.account.id,
+            'account_id': account.id,
             'move_id': self.move.id,
             'analytic_account_id': self.analytic_account_id,
             'analytic_tag_ids': [(6, 0, [self.analytic_tag_type_a.id,
-                                         self.analytic_tag_concept_a.id,
-                                         self.analytic_tag_test_a.id])]
+                                         self.analytic_tag_concept_a.id])]
         }
-        line = self.move_line_obj.create(values)
+        move_line_obj = self.env['account.move.line']
+        line = move_line_obj.create(values)
         self.assertTrue(
             line.x_dimension_type.id == self.analytic_tag_type_a.id)
         self.assertTrue(
             line.x_dimension_concept.id == self.analytic_tag_concept_a.id)
+        values_update = {
+            'analytic_tag_ids': [(6, 0, [self.analytic_tag_type_a.id,
+                                         self.analytic_tag_type_b.id])]
+        }
+        with self.assertRaises(ValidationError):
+            line.write(values_update)
+
+    def test_invoice_line_dimension(self):
+        """Test dimension creation on account invoice line creation
+        """
+        partner = self.env.ref('base.res_partner_2')
+        account = self.account_obj.create({
+            'code': 'test_dimension_acc_02',
+            'name': 'test dimension account',
+            'user_type_id': self.account_user_type.id,
+            'reconcile': True,
+        })
+        self.invoice = self.env['account.invoice'].create({
+            'journal_id': self.journal.id,
+            'company_id': self.company_id,
+            'partner_id': partner.id
+        })
+        values = {
+            'name': 'test',
+            'price_unit': 1,
+            'account_id': account.id,
+            'invoice_id': self.invoice.id,
+            'analytic_account_id': self.analytic_account_id,
+            'analytic_tag_ids': [(6, 0, [self.analytic_tag_type_a.id,
+                                         self.analytic_tag_concept_a.id])]
+        }
+        invoice_line_obj = self.env['account.invoice.line']
+        line = invoice_line_obj.create(values)
+        self.assertTrue(
+            line.x_dimension_type.id == self.analytic_tag_type_a.id)
+        self.assertTrue(
+            line.x_dimension_concept.id == self.analytic_tag_concept_a.id)
+        values_update = {
+            'analytic_tag_ids': [(6, 0, [self.analytic_tag_type_a.id])]
+        }
+        line.write(values_update)
         values_update = {
             'analytic_tag_ids': [(6, 0, [self.analytic_tag_type_a.id,
                                          self.analytic_tag_type_b.id])]
