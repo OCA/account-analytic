6e6a1a29c976b1e56e25b231284ac63bae38c10e
cubells <vicent@vcubells.net>
2017-04-26 19:17:59 +0200

    [ADD][9.0] account_analytic_distribution module

---
+++ account_analytic_distribution/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_analytic_distribution
+from . import account_move_line
+from . import account_invoice
+from . import account_invoice_line
+from . import res_company
