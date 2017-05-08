d872d75501030bbce83474703219ec7a97b04824
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-05-08 15:26:57 +0200

    [MIG][10.0] pos_analytic_by_config

--- pos_analytic_by_config/models/pos_order.py
+++ pos_analytic_by_config/models/pos_order.py
@@ -2,7 +2,7 @@
 # © 2015 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class PosOrder(models.Model):
