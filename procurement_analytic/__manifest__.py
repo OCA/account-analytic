f999f755ff6ece655b51d56f6ddfbf23b47e5124
cubells <vicent@vcubells.net>
2017-06-22 12:47:35 +0200

    [MIG] procurement_analytic module: migration to 10.0

--- procurement_analytic/__manifest__.py
+++ procurement_analytic/__manifest__.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
-# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Procurement Analytic',
     'summary': 'This module adds analytic account to procurements',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Analytic',
     'license': 'AGPL-3',
     'author': "Tecnativa, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.tecnativa.com',
-    'depends': ['procurement'],
+    'website': 'https://www.tecnativa.com',
+    'depends': [
+        'procurement',
+    ],
     'data': [
         'views/procurement_analytic.xml',
     ],
