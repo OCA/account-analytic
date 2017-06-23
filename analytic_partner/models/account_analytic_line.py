6e1e880089d3f859d8b68cd337cecdef3ca7816c
OCA Transbot <transbot@odoo-community.org>
2017-06-24 00:01:22 +0200

    [MIG] analytic_account: Migrated to 10.0
    OCA Transbot updated translations from Transifex

--- analytic_partner/models/account_analytic_line.py
+++ analytic_partner/models/account_analytic_line.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Pedro M. Baeza
+# Copyright 2015 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields
+
+from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    partner_id = fields.Many2one(
-        comodel_name='res.partner', string="Account Partner",
-        related="account_id.partner_id", readonly=True, store=True)
     other_partner_id = fields.Many2one(
         comodel_name='res.partner', string="Other Partner",
         domain="['|', ('parent_id', '=', False), ('is_company', '=', True)]")
