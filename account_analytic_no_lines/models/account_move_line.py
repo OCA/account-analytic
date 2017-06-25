9b76c93378deb3b51e4f98a60a45c86069c41051
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-06-25 13:57:56 +0200

    [IMP] account_analytic_no_lines: add hook
    Add hook to determine if analytic lines must be created or not.
    The default provided hook tests if the line is about a expensable
    product that must be invoiced to the customer, in which case the
    analytic line must be generated so it is added to the sale order.

--- account_analytic_no_lines/models/account_move_line.py
+++ account_analytic_no_lines/models/account_move_line.py
@@ -9,6 +9,17 @@ class AccountMoveLine(models.Model):
 
     _inherit = 'account.move.line'
 
+    @api.model
+    def _must_create_analytic_line(self, move_line):
+        if not move_line.product_id:
+            return False
+        if not hasattr(move_line.product_id, 'expense_policy'):
+            return False
+        if move_line.product_id.expense_policy == 'no':
+            return False
+        return True
+
     @api.multi
     def create_analytic_lines(self):
-        pass
+        self = self.filtered(self._must_create_analytic_line)
+        return super(AccountMoveLine, self).create_analytic_lines()
