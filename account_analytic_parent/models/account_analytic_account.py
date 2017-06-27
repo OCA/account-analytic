1f2d000dba133af0e271a2995e45806c8610d413
David Vidal <chienandalu@gmail.com>
2017-06-27 15:58:12 +0200

    [MIG] account_analytic_parent: Migration to 10.0

--- account_analytic_parent/models/account_analytic_account.py
+++ account_analytic_parent/models/account_analytic_account.py
@@ -5,8 +5,9 @@
 # Copyright 2017 Deneroteam.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models, _
-from openerp.exceptions import ValidationError
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class AccountAnalyticAccount(models.Model):
@@ -27,46 +28,34 @@ class AccountAnalyticAccount(models.Model):
         """
         super(AccountAnalyticAccount, self)._compute_debit_credit_balance()
         for account in self:
-            debit = account.debit
-            credit = account.credit
-            balance = account.balance
-            for child in account.child_ids:
-                debit += child.debit
-                credit += child.credit
-                balance += child.balance
-            account.debit = debit
-            account.credit = credit
-            account.balance = balance
+            account.debit += sum(account.mapped('child_ids.debit'))
+            account.credit += sum(account.mapped('child_ids.credit'))
+            account.balance += sum(account.mapped('child_ids.balance'))
 
+    @api.multi
     @api.constrains('parent_id')
-    @api.one
     def check_recursion(self):
-        if not super(AccountAnalyticAccount, self)._check_recursion():
-            raise ValidationError(
-                _('You can not create recursive analytic accounts.'),
-            )
+        for account in self:
+            if not super(AccountAnalyticAccount, account)._check_recursion():
+                raise UserError(
+                    _('You can not create recursive analytic accounts.'),
+                )
 
     @api.multi
     @api.onchange('parent_id')
-    def on_change_parent(self):
+    def _onchange_parent_id(self):
         for account in self:
-            account.partner_id = account.parent_id.partner_id or False
+            account.partner_id = account.parent_id.partner_id
 
     @api.multi
     @api.depends('name')
     def name_get(self):
         res = []
-        for account_item in self:
-            data = []
-            proj = account_item
-
-            while proj:
-                if proj and proj.name:
-                    data.insert(0, proj.name)
-                else:
-                    data.insert(0, '')
-
-                proj = proj.parent_id
-            data = ' / '.join(data)
-            res.append((account_item.id, data))
+        for account in self:
+            current = account
+            name = current.name
+            while current.parent_id:
+                name = '%s / %s' % (current.parent_id.name, name)
+                current = current.parent_id
+            res.append((account.id, name))
         return res
