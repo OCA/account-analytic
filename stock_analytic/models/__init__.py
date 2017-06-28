c72e08a4a0c2fff3d930fad280481dabf1295294
aheficent <ahenriquez@eficent.com>
2017-06-28 14:03:17 +0200

    [MIG]stock_analytic to v10

--- stock_analytic/models/__init__.py
+++ stock_analytic/models/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013 Julius Network Solutions
-# Copyright 2015 Clear Corp
-# Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import stock
