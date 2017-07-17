f4cb21a34a1bdc8b60e9a70b9e718674620b3276
Alexis de Lattre <alexis.delattre@akretion.com>
2017-07-17 23:27:22 +0200

    product_analytic: add method _get_product_analytic_accounts() similar to the one _get_product_accounts() of the account module
    product_analytic: add demo data
    Add modules product_analytic_pos and product_analytic_purchase

--- product_analytic/models/account_invoice.py
+++ product_analytic/models/account_invoice.py
@@ -6,6 +6,13 @@
 
 from odoo import api, models
 
+INV_TYPE_MAP = {
+    'out_invoice': 'income',
+    'out_refund': 'income',
+    'in_invoice': 'expense',
+    'in_refund': 'expense',
+}
+
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
@@ -13,32 +20,23 @@ class AccountInvoiceLine(models.Model):
     @api.onchange('product_id')
     def _onchange_product_id(self):
         res = super(AccountInvoiceLine, self)._onchange_product_id()
-        type = self.invoice_id.type
-        product = self.product_id
-        if product:
-            if type in ('out_invoice', 'out_refund'):
-                self.account_analytic_id =\
-                    product.income_analytic_account_id.id or\
-                    product.categ_id.income_analytic_account_id.id
-            else:
-                self.account_analytic_id =\
-                    product.expense_analytic_account_id.id or\
-                    product.categ_id.expense_analytic_account_id.id
+        inv_type = self.invoice_id.type
+        if self.product_id:
+            ana_accounts = self.product_id.product_tmpl_id.\
+                _get_product_analytic_accounts()
+            ana_account = ana_accounts[INV_TYPE_MAP[inv_type]]
+            self.account_analytic_id = ana_account.id
         return res
 
     @api.model
     def create(self, vals):
-        type = self.env.context.get('inv_type', 'out_invoice')
-        if vals.get('product_id') and type and \
+        inv_type = self.env.context.get('inv_type', 'out_invoice')
+        if vals.get('product_id') and inv_type and \
                 not vals.get('account_analytic_id'):
             product = self.env['product.product'].browse(
                 vals.get('product_id'))
-            if type in ('out_invoice', 'out_refund'):
-                analytic_id = product.income_analytic_account_id.id or\
-                    product.categ_id.income_analytic_account_id.id
-            else:
-                analytic_id = product.expense_analytic_account_id.id or\
-                    product.categ_id.expense_analytic_account_id.id
-            if analytic_id:
-                vals['account_analytic_id'] = analytic_id
+            ana_accounts = product.product_tmpl_id.\
+                _get_product_analytic_accounts()
+            ana_account = ana_accounts[INV_TYPE_MAP[inv_type]]
+            vals['account_analytic_id'] = ana_account.id
         return super(AccountInvoiceLine, self).create(vals)
