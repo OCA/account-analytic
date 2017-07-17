270113dec13d96ee2872691a147416a8e6936244
Alexis de Lattre <alexis.delattre@akretion.com>
2017-07-17 23:27:22 +0200

    product_analytic: add method _get_product_analytic_accounts() similar to the one _get_product_accounts() of the account module
    product_analytic: add demo data
    Add modules product_analytic_pos and product_analytic_purchase

---
+++ product_analytic_pos/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com/) - Alexis de Lattre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product Analytic POS',
+    'version': '10.0.1.0.0',
+    'category': 'Point Of Sale',
+    'license': 'AGPL-3',
+    'summary': 'Glue module between product_analytic and point_of_sale',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['product_analytic', 'point_of_sale'],
+    'auto_install': True,
+    'installable': True,
+}
