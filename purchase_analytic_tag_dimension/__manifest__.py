47e7a1669ff5d618635717f1e12b9e57db1580dd
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-08-23 17:52:24 +0200

    [add] purchase and sale analytic tag dimension

---
+++ purchase_analytic_tag_dimension/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Analytic Tag Dimension",
+    'summary': "Group Analytic Entries by Dimensions",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA,Odoo Community Association (OCA)",
+    "website": "http://www.camptocamp.com",
+    "depends": [
+        'analytic_tag_dimension',
+        'purchase'
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+}
