bfc81e76c6bd7d92bdc3c1dfe65b762b7f955d06
cubells <vicent@vcubells.net>
2017-09-14 19:02:10 +0200

    [MIG] purchase_procurement_analytic module: migration to 10.0

--- purchase_procurement_analytic/models/purchase.py
+++ purchase_procurement_analytic/models/purchase.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from odoo import models
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    def search(self, cr, uid, args, offset=0, limit=None, order=None,
-               context=None, count=False):
-        if 'account_analytic_id' in context and (
-                {'order_id', 'product_id', 'product_uom'} <=
-                set(x[0] for x in args)):
-            args.insert(0, (
-                'account_analytic_id', '=', context['account_analytic_id']))
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        if 'limit_procurement_account_analytic_id' in self.env.context:
+            args.insert(0, ('account_analytic_id', '=', self.env.context.get(
+                'limit_procurement_account_analytic_id')))
         return super(PurchaseOrderLine, self).search(
-            cr, uid, args, offset=offset, limit=limit, order=order,
-            context=context, count=count)
+            args, offset=offset, limit=limit, order=order, count=count)
