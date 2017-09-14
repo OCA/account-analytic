c2da3db4f33e8032b5ea74790338fb61dc6c96ca
cubells <vicent@vcubells.net>
2017-09-14 13:39:45 +0200

    [ADD] sale_procurement_analytic: Migration to 10.0

---
+++ sale_procurement_analytic/models/sale.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def _prepare_order_line_procurement(self, group_id=False):
+        self.ensure_one()
+        res = super(SaleOrderLine, self)._prepare_order_line_procurement(
+            group_id=group_id)
+        # project_id field contains actually the analytic account, not the
+        # project
+        res['account_analytic_id'] = self.order_id.project_id.id
+        return res
