2d4840ccb4b7f02607213c5f63a11d72e6397ed0
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-10-16 15:41:07 +0200

    Rename modules and add category to manifest

---
+++ analytic_tag_dimension_sale_warning/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Analytic Tag Dimension Sale Warning",
+    "summary": "Group Analytic Entries by Dimensions",
+    "category": "Generic Modules/Accounting",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA,Odoo Community Association (OCA)",
+    "website": "http://www.camptocamp.com",
+    "depends": [
+        'analytic_tag_dimension',
+        'sale'
+    ],
+    "installable": True,
+    "auto_install": True,
+}
