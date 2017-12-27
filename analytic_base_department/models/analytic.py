6e334b5e9600cb5c5177e0dfc44eaeb9fe13972d
Artem Kostyuk <a.kostyuk@mobilunity.com>
2017-12-27 12:31:59 +0200

    [MIG] analytic_base_department: Migrate to 11.0

--- analytic_base_department/models/analytic.py
+++ analytic_base_department/models/analytic.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2011-2016 Camptocamp SA
-# © 2013 Daniel Reis
+# Copyright 2011-2016 Camptocamp SA
+# Copyright 2013 Daniel Reis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
 
@@ -16,19 +15,18 @@ class AnalyticAccount(models.Model):
 class AnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    @api.multi
-    def _get_department(self):
-        emp_model = self.env['hr.employee']
+    @api.model
+    def _default_department(self):
         department_id = False
-        employee = emp_model.search([('user_id', '=', self.env.uid)], limit=1)
-        if employee and employee.department_id:
-            department_id = employee.department_id.id
+        employee = self.env.user.employee_ids
+        if employee and employee[0].department_id:
+            department_id = employee[0].department_id.id
         return department_id
 
     department_id = fields.Many2one(
         'hr.department',
         'Department',
-        default=_get_department,
+        default=lambda self: self._default_department(),
         help="User's related department")
     account_department_id = fields.Many2one(
         comodel_name='hr.department',
