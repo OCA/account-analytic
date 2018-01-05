5d9207caf3ff9a0fd72774f204e5087c4354aaa9
mpanarin <m.panarin@mobilunity.com>
2018-01-05 13:17:13 +0200

    [MIG] analytic_tag_dimension_sale_warning: Odoo 11

--- analytic_tag_dimension_sale_warning/__manifest__.py
+++ analytic_tag_dimension_sale_warning/__manifest__.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
+# Copyright 2018 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Analytic Tag Dimension Sale Warning",
     "summary": "Group Analytic Entries by Dimensions",
     "category": "Generic Modules/Accounting",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA,Odoo Community Association (OCA)",
-    "website": "http://www.camptocamp.com",
+    "website": "https://github.com/OCA/account-analytic",
     "depends": [
         'analytic_tag_dimension',
-        'sale'
+        'sale',
     ],
-    "installable": True,
     "auto_install": True,
 }
