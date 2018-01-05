f222394b56812f3b55e4edcea1ae068d4045c6c1
mpanarin <m.panarin@mobilunity.com>
2018-01-05 15:52:25 +0200

    [MIG] analytic_tag_dimension_purchase_warning: migrate odoo 11

--- analytic_tag_dimension_purchase_warning/__manifest__.py
+++ analytic_tag_dimension_purchase_warning/__manifest__.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
+# Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Analytic Tag Dimension Purchase Warning",
     "summary": "Group Analytic Entries by Dimensions",
     "category": "Generic Modules/Accounting",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptocamp SA,Odoo Community Association (OCA)",
-    "website": "http://www.camptocamp.com",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "http://github.com/OCA/account-analytic",
     "depends": [
         'analytic_tag_dimension',
-        'purchase'
+        'purchase',
     ],
-    "installable": True,
     "auto_install": True,
 }
