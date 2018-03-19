f71b4ab441364a7e10e467ae0a344588a2601735
Jared Kipe <jared@hibou.io>
2018-03-19 08:47:24 -0700

    11.0 MIG stock_analytic

--- stock_analytic/models/stock.py
+++ stock_analytic/models/stock.py
@@ -3,6 +3,7 @@
 # Copyright 2015 Clear Corp
 # Copyright 2016 OpenSynergy Indonesia
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Hibou Corp.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models, api
