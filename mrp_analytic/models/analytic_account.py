e7627a6af6c496726f11cd6d466000f3567c866b
oca-travis <oca+oca-travis@odoo-community.org>
2018-06-29 12:22:02 +0000

    [MIG]mrp_analytic to v10
    [FIX] mrp_analytic: openerp => odoo
    
    [UPD] Update mrp_analytic.pot

--- mrp_analytic/models/analytic_account.py
+++ mrp_analytic/models/analytic_account.py
@@ -2,7 +2,7 @@
 # © 2015 Pedro M. Baeza - Antiun Ingeniería
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, models, fields
+from odoo import api, models, fields
 
 
 class AnalyticAccount(models.Model):
--- mrp_analytic/models/mrp_production.py
+++ mrp_analytic/models/mrp_production.py
@@ -2,7 +2,7 @@
 # © 2015 Eficent - Jordi Ballester Alomar
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MrpProduction(models.Model):
