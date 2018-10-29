e79fe1993c2785b522c26c50135b1d357466baa0
Beñat Jimenez <benat.jimenez1994@gmail.com>
2018-10-29 11:49:17 +0100

    [MIG] account_analytic_sequence to v12.0

--- account_analytic_sequence/__manifest__.py
+++ account_analytic_sequence/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Account Analytic Sequence',
     'summary': """
         Restore the analytic account sequence""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://www.acsone.eu',
