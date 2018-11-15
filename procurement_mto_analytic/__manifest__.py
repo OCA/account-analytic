11a32afc4433b8e7fc174e05352180ce8b9e2e5f
Sergej Lozikov <sergej@ventor.tech>
2018-11-15 17:31:05 +0000

    [FIX][11.0] Rename purchase_procurement_analytic module

--- procurement_mto_analytic/__manifest__.py
+++ procurement_mto_analytic/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Purchase Procurement Analytic',
+    'name': 'Purchase Analytic (MTO)',
     'summary': 'This module sets analytic account in purchase order line from '
                'sale order analytic account',
     'version': '11.0.1.0.0',
