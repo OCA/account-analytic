14fc63c057042d8d88b9f6cd8676c013dea7f20c
Anand Kansagra <kansagraanand@hotmail.com>
2018-12-04 10:10:13 +0530

    [MIG] analytic_base_department: Migration to 12.0.

--- analytic_base_department/__manifest__.py
+++ analytic_base_department/__manifest__.py
@@ -3,13 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Base Analytic Department Categorization",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
     "website": "https://github.com/OCA/account-analytic",
     "depends": ["account",
-                "analytic",
                 "hr"],
     "data": ["views/analytic.xml"],
     "installable": True,
