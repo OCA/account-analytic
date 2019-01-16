7ebddc9a6d045dc4ef73f09983bd10aaf71f2aee
hveficent <hector.villarreal@eficent.com>
2019-01-16 11:02:57 +0100

    [11.0][ADD] stock_inventory_analytic

---
+++ stock_inventory_analytic/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': "Stock Inventory Analytic",
+    'summary': """
+        Stock Inventory Analytic """,
+    'author': 'Eficent, Odoo Community Association (OCA)',
+    'website': "https://github.com/OCA/analytic-account",
+    'category': 'Warehouse Management',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock_analytic',
+    ],
+    'data': [
+        'views/stock_inventory_line_view.xml',
+        'wizard/stock_product_change_qty.xml',
+    ],
+    'installable': True,
+}
