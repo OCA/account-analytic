8487382d3031dd56550509b87e82e72f4753bcc6
Aaron Henriquez <ahenriquez@eficent.com>
2019-01-30 17:52:17 +0100

    [MIG]stock_analytic to v12

--- stock_analytic/models/stock.py
+++ stock_analytic/models/stock.py
@@ -23,7 +23,7 @@ class StockMove(models.Model):
         res = super(StockMove, self)._prepare_account_move_line(
             qty, cost, credit_account_id, debit_account_id)
         # Add analytic account in debit line
-        if not self.analytic_account_id:
+        if not self.analytic_account_id or not res:
             return res
 
         for num in range(0, 2):
@@ -33,3 +33,10 @@ class StockMove(models.Model):
                     'analytic_account_id': self.analytic_account_id.id,
                 })
         return res
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    analytic_account_id = fields.Many2one(
+        related='move_id.analytic_account_id')
