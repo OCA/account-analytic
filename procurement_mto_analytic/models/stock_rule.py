3157a79f177aa808a6aa70ecc3fcedb8b89f472e
Duc Dao <duc.dd@komit-consulting.com>
2019-02-18 12:34:49 +0700

    [MIG] procurement_mto_analytic: Migration to 12.0

--- procurement_mto_analytic/models/stock_rule.py
+++ procurement_mto_analytic/models/stock_rule.py
@@ -3,15 +3,14 @@
 from odoo import api, models
 
 
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
     @api.multi
     def _prepare_purchase_order_line(self, product_id, product_qty,
                                      product_uom, values, po, supplier):
-        res = super(ProcurementRule, self).\
-            _prepare_purchase_order_line(product_id, product_qty,
-                                         product_uom, values, po, supplier)
+        res = super(StockRule, self)._prepare_purchase_order_line(
+            product_id, product_qty, product_uom, values, po, supplier)
         res.update({
             'account_analytic_id':
                 values.get('account_analytic_id', False)
@@ -19,8 +18,8 @@ class ProcurementRule(models.Model):
         return res
 
     def _make_po_get_domain(self, values, partner):
-        res = super(ProcurementRule, self).\
-            _make_po_get_domain(values, partner)
+        res = super(StockRule, self)._make_po_get_domain(
+            values, partner)
         res += (('order_line.account_analytic_id', '=',
                  values.get('account_analytic_id', False)),)
         return res
