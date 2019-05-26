d1500cfcbf0c472c8bd24f7f05fc2582cf009a77
tbaden <thore.baden@gmx.net>
2019-05-26 14:12:30 +0200

    [12.0][MIG] product_analytic

--- product_analytic/models/account_invoice.py
+++ product_analytic/models/account_invoice.py
@@ -18,7 +18,7 @@ class AccountInvoiceLine(models.Model):
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
-        res = super(AccountInvoiceLine, self)._onchange_product_id()
+        res = super()._onchange_product_id()
         inv_type = self.invoice_id.type
         if self.product_id:
             ana_accounts = self.product_id.product_tmpl_id.\
@@ -27,15 +27,16 @@ class AccountInvoiceLine(models.Model):
             self.account_analytic_id = ana_account.id
         return res
 
-    @api.model
-    def create(self, vals):
-        inv_type = self.env.context.get('inv_type', 'out_invoice')
-        if vals.get('product_id') and inv_type and \
-                not vals.get('account_analytic_id'):
-            product = self.env['product.product'].browse(
-                vals.get('product_id'))
-            ana_accounts = product.product_tmpl_id.\
-                _get_product_analytic_accounts()
-            ana_account = ana_accounts[INV_TYPE_MAP[inv_type]]
-            vals['account_analytic_id'] = ana_account.id
-        return super(AccountInvoiceLine, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            inv_type = self.env.context.get('inv_type', 'out_invoice')
+            if vals.get('product_id') and inv_type and \
+                    not vals.get('account_analytic_id'):
+                product = self.env['product.product'].browse(
+                    vals.get('product_id'))
+                ana_accounts = product.product_tmpl_id.\
+                    _get_product_analytic_accounts()
+                ana_account = ana_accounts[INV_TYPE_MAP[inv_type]]
+                vals['account_analytic_id'] = ana_account.id
+        return super().create(vals_list)
