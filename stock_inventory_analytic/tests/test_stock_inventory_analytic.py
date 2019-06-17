e446cfa4c7fb8559409279b6bf367dbf7b3ac1ae
BT-cserra <carlos.serra@braintec-group.com>
2019-06-17 14:41:58 +0200

    [MIG] stock_inventory_analytic: Migration to 12.0

--- stock_inventory_analytic/tests/test_stock_inventory_analytic.py
+++ stock_inventory_analytic/tests/test_stock_inventory_analytic.py
@@ -1,35 +1,59 @@
 # Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 brain-tec AG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestInventoryAnalytic(SavepointCase):
+class TestInventoryAnalytic(TransactionCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestInventoryAnalytic, cls).setUpClass()
+    def setUp(self):
+        super(TestInventoryAnalytic, self).setUp()
 
         # MODELS
-        cls.product_product_model = cls.env['product.product']
-        cls.product_category_model = cls.env['product.category']
-        cls.wizard_model = cls.env['stock.change.product.qty']
+        self.product_product_model = self.env['product.product']
+        self.product_category_model = self.env['product.category']
+        self.wizard_model = self.env['stock.change.product.qty']
 
         # INSTANCES
-        cls.category = cls.product_category_model.create({
+        self.category = self.product_category_model.create({
             'name': 'Physical (test)',
             'property_cost_method': 'standard',
             'property_valuation': 'real_time',
         })
-        cls.analytic_account = cls.env.ref(
+        self.analytic_account = self.env.ref(
             'analytic.analytic_agrolait')
 
+        # Accounts for the product & product's category.
+        account_group = self.env['account.group'].create({
+            'name': 'Account Group (test)',
+            'code_prefix': 'AGTest-',
+        })
+        user_type = self.env.ref('account.data_account_type_liquidity')
+        self.account_account_70000 = self.env['account.account'].create({
+            'code': '70000',
+            'name': '70000 (test)',
+            'group_id': account_group.id,
+            'user_type_id': user_type.id,
+        })
+        self.account_account_70001 = self.env['account.account'].create({
+            'code': '70001',
+            'name': '70001 (test)',
+            'group_id': account_group.id,
+            'user_type_id': user_type.id,
+        })
+
     def _create_product(self, name):
+        self.category.property_stock_valuation_account_id = \
+            self.account_account_70000.id
         return self.product_product_model.create({
             'name': name,
             'categ_id': self.category.id,
             'type': 'product',
-            'standard_price': 100, })
+            'standard_price': 100,
+            'property_stock_account_input': self.account_account_70000.id,
+            'property_stock_account_output': self.account_account_70001.id,
+        })
 
     def _product_change_qty(self, product, new_qty,
                             analytic_account_id=None):
@@ -45,20 +69,35 @@ class TestInventoryAnalytic(SavepointCase):
     def test_product_change_qty_analytic(self):
         product = self._create_product('product_product')
 
-        analytic_lines_before = self.env['account.analytic.line'].search(
-            [('product_id', '=', product.id),
-             ('account_id', '=', self.analytic_account.id)])
+        inventory_lines_before = self.env['stock.inventory.line'].search([
+            ('product_id', '=', product.id),
+            ('analytic_account_id', '=', self.analytic_account.id),
+        ])
+        analytic_lines_before = self.env['account.analytic.line'].search([
+            ('product_id', '=', product.id),
+            ('account_id', '=', self.analytic_account.id),
+        ])
 
         self._product_change_qty(product, 10, self.analytic_account)
 
-        analytic_lines_after = self.env['account.analytic.line'].search(
-            [('product_id', '=', product.id),
-             ('account_id', '=', self.analytic_account.id)])
+        # Checks that there exists an inventory line created with that account,
+        # and which belongs to an inventory adjustment that has been validated.
+        inventory_lines_after = self.env['stock.inventory.line'].search([
+            ('product_id', '=', product.id),
+            ('analytic_account_id', '=', self.analytic_account.id),
+        ])
+        self.assertNotEqual(inventory_lines_before, inventory_lines_after)
+        inventory_line_created = inventory_lines_after - inventory_lines_before
+        self.assertEqual(inventory_line_created.inventory_id.state, 'done')
 
+        # Checks that there exists two analytic lines created with that account
+        analytic_lines_after = self.env['account.analytic.line'].search([
+            ('product_id', '=', product.id),
+            ('account_id', '=', self.analytic_account.id),
+        ])
         self.assertNotEqual(analytic_lines_before, analytic_lines_after)
-
-        analytic_line_created = analytic_lines_after - analytic_lines_before
-
-        self.assertEqual(analytic_line_created.unit_amount, 10)
-        self.assertEqual(analytic_line_created.amount,
-                         product.standard_price * 10)
+        analytic_lines_created = analytic_lines_after - analytic_lines_before
+        self.assertEqual(sorted(analytic_lines_created.mapped('amount')),
+                         [-1000.0, +1000.0])
+        self.assertEqual(analytic_lines_created.mapped('unit_amount'),
+                         [10.0, 10.0])
