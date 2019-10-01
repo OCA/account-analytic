993f70fb605170f0ad3ff36f0086615d42347da1
Jim Hoefnagels <jim.hoefnagels@dynapps.be>
2019-10-01 11:12:31 +0200

    [MIG] account_analytic_required: Migration to 13.0

--- account_analytic_required/__manifest__.py
+++ account_analytic_required/__manifest__.py
@@ -2,13 +2,13 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
-    'name': 'Account Analytic Required',
-    'version': '12.0.1.0.0',
-    'category': 'Analytic Accounting',
-    'license': 'AGPL-3',
-    'author': "Akretion, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/account-analytic',
-    'depends': ['account', 'account_type_menu'],
-    'data': ['views/account.xml'],
-    'installable': True,
+    "name": "Account Analytic Required",
+    "version": "13.0.1.0.0",
+    "category": "Analytic Accounting",
+    "license": "AGPL-3",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": ["account", "account_menu"],
+    "data": ["views/account.xml"],
+    "installable": True,
 }
