fcc47433d0999ae26b8c2202f1d4a98e5d2aabed
mreficent <miquel.raich@eficent.com>
2019-10-23 16:38:51 +0200

    [MIG] purchase_analytic: Migration to 12.0

--- purchase_analytic/__manifest__.py
+++ purchase_analytic/__manifest__.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
 # © 2016  Laetitia Gangloff, Acsone SA/NV (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Purchase Analytic",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     'author': "Acsone SA/NV,Odoo Community Association (OCA)",
     "category": "Purchase Management",
-    "website": "http://www.acsone.eu",
-    "depends": ["purchase"
-                ],
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "purchase",
+    ],
     "data": ["views/purchase_views.xml"],
     "license": "AGPL-3",
     "installable": True,
