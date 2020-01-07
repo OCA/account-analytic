9360db0b450ea9f2de84c16fb3b87ba8bf7605b7
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-07 17:09:43 +0700

    [MIG] analytic_partner: Migration to 13.0

--- analytic_partner/models/account_move_line.py
+++ analytic_partner/models/account_move_line.py
@@ -2,14 +2,13 @@
 # Copyright 2017 Tecnativa - Luis Martínez
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.multi
     def _prepare_analytic_line(self):
         res = super(AccountMoveLine, self)._prepare_analytic_line()
-        res[0]["other_partner_id"] = self.invoice_id.partner_id.commercial_partner_id.id
+        res[0]["other_partner_id"] = self.move_id.partner_id.commercial_partner_id.id
         return res
