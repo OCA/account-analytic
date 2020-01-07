a622eb640104cdf0cc3d8223613b1f84ac0ac8a8
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-07 14:56:41 +0700

    [IMP] analytic_partner: black, isort

--- analytic_partner/models/account_move_line.py
+++ analytic_partner/models/account_move_line.py
@@ -6,11 +6,10 @@ from odoo import api, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     @api.multi
     def _prepare_analytic_line(self):
         res = super(AccountMoveLine, self)._prepare_analytic_line()
-        res[0]['other_partner_id'] = \
-            self.invoice_id.partner_id.commercial_partner_id.id
+        res[0]["other_partner_id"] = self.invoice_id.partner_id.commercial_partner_id.id
         return res
