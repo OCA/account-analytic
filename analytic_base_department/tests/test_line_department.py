1d4469f28c64321aab53cf6a804291a91023917c
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-07 14:43:17 +0700

    [MIG] analytic_base_department: Migration to 13.0

--- analytic_base_department/tests/test_line_department.py
+++ analytic_base_department/tests/test_line_department.py
@@ -13,13 +13,13 @@ class LineDepartmentCase(TransactionCase):
 
     def test_default_department(self):
         """In a new users form, a user set only the firstname."""
-        aal = self.env["account.analytic.line"].sudo(self.user).new()
+        aal = self.env["account.analytic.line"].with_user(self.user).new()
         department_id = aal.default_get(["department_id"]).get("department_id")
         self.assertEqual(department_id, self.dep.id)
 
     def test_no_employees(self):
         """Tesing lines created by users w/out employee records."""
         self.user.employee_ids = [(5, 0)]
-        aal = self.env["account.analytic.line"].sudo(self.user).new()
+        aal = self.env["account.analytic.line"].with_user(self.user).new()
         department_id = aal.default_get(["department_id"]).get("department_id")
         self.assertFalse(department_id)
