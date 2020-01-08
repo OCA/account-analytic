ca3964674ff6631a2171d60d447e7c3bfcb60c2f
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-08 11:22:26 +0700

    [IMP] procurement_mto_analytic: black, isort

--- procurement_mto_analytic/models/sale_order_line.py
+++ procurement_mto_analytic/models/sale_order_line.py
@@ -4,26 +4,18 @@ from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.multi
     def _prepare_procurement_values(self, group_id=False):
-        res = super(SaleOrderLine, self).\
-            _prepare_procurement_values(group_id)
-        res.update({
-            'account_analytic_id':
-                self.order_id.analytic_account_id.id,
-        })
+        res = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
+        res.update({"account_analytic_id": self.order_id.analytic_account_id.id})
         return res
 
     @api.multi
-    def _purchase_service_prepare_line_values(self, purchase_order,
-                                              quantity=False):
-        res = super(SaleOrderLine, self).\
-            _purchase_service_prepare_line_values(
-                purchase_order=purchase_order, quantity=quantity)
-        res.update({
-            'account_analytic_id':
-                self.order_id.analytic_account_id.id,
-        })
+    def _purchase_service_prepare_line_values(self, purchase_order, quantity=False):
+        res = super(SaleOrderLine, self)._purchase_service_prepare_line_values(
+            purchase_order=purchase_order, quantity=quantity
+        )
+        res.update({"account_analytic_id": self.order_id.analytic_account_id.id})
         return res
