31d0fabfbe7cfe52a0c2954fcde9a461b9da861f
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-08 09:40:46 +0700

    [IMP] mrp_analytic: black, isort

--- mrp_analytic/models/analytic_account.py
+++ mrp_analytic/models/analytic_account.py
@@ -8,9 +8,9 @@ class AnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
 
     def _compute_num_productions(self):
-        production = self.env['mrp.production']
+        production = self.env["mrp.production"]
         for analytic_account in self:
-            domain = [('analytic_account_id', '=', analytic_account.id)]
+            domain = [("analytic_account_id", "=", analytic_account.id)]
             analytic_account.num_productions = production.search_count(domain)
 
     num_productions = fields.Integer(compute="_compute_num_productions")
