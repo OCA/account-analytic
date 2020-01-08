b37a3ef493615b765c95b9908fb64842eb00d1cc
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-08 09:40:46 +0700

    [IMP] mrp_analytic: black, isort

--- mrp_analytic/__manifest__.py
+++ mrp_analytic/__manifest__.py
@@ -3,21 +3,18 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Analytic for manufacturing',
-    'summary': 'Adds the analytic account to the production order',
-    'version': '12.0.1.0.0',
-    'category': "Manufacturing",
-    'author': 'Eficent, '
-              'Antiun Ingeniería, '
-              'Serv. Tecnol. Avanzados - Pedro M. Baeza, '
-              'José L. Sandoval A., '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-analytic',
-    'license': 'AGPL-3',
-    'depends': ['mrp', 'analytic'],
-    'data': [
-        "views/mrp_view.xml",
-        "views/analytic_account_view.xml",
-    ],
-    'installable': True,
+    "name": "Analytic for manufacturing",
+    "summary": "Adds the analytic account to the production order",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Eficent, "
+    "Antiun Ingeniería, "
+    "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "José L. Sandoval A., "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "license": "AGPL-3",
+    "depends": ["mrp", "analytic"],
+    "data": ["views/mrp_view.xml", "views/analytic_account_view.xml"],
+    "installable": True,
 }
--- mrp_analytic/tests/test_mrp_analytic.py
+++ mrp_analytic/tests/test_mrp_analytic.py
@@ -5,26 +5,26 @@ from odoo.tests import common
 
 
 class TestMrpAnalytic(common.TransactionCase):
-
     def setUp(self):
         super(TestMrpAnalytic, self).setUp()
-        self.analytic_account = self.env['account.analytic.account'].create(
-            {'name': 'Analytic account test'})
-        self.product = self.env['product.product'].create({
-            'name': 'Test product',
-        })
-        self.bom = self.env['mrp.bom'].create(
+        self.analytic_account = self.env["account.analytic.account"].create(
+            {"name": "Analytic account test"}
+        )
+        self.product = self.env["product.product"].create({"name": "Test product"})
+        self.bom = self.env["mrp.bom"].create(
             {
-                'product_id': self.product.id,
-                'product_tmpl_id': self.product.product_tmpl_id.id,
-            })
-        self.production = self.env['mrp.production'].create(
+                "product_id": self.product.id,
+                "product_tmpl_id": self.product.product_tmpl_id.id,
+            }
+        )
+        self.production = self.env["mrp.production"].create(
             {
-                'product_id': self.product.id,
-                'analytic_account_id': self.analytic_account.id,
-                'product_uom_id': self.product.uom_id.id,
-                'bom_id': self.bom.id,
-            })
+                "product_id": self.product.id,
+                "analytic_account_id": self.analytic_account.id,
+                "product_uom_id": self.product.uom_id.id,
+                "bom_id": self.bom.id,
+            }
+        )
 
     def test_num_productions(self):
         self.assertEqual(self.analytic_account.num_productions, 1)
