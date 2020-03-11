6f14cd8b11fc92663fb70db8e1603f542b7883b1
Iván Todorovich <ivan.todorovich@gmail.com>
2020-03-11 14:06:02 +0100

    [IMP] use openupgradelib

--- account_analytic_required/migrations/12.0.2.0.0/post-migrate.py
+++ account_analytic_required/migrations/12.0.2.0.0/post-migrate.py
@@ -1,55 +1,15 @@
-from odoo import tools
+# Copyright 2020 Druidoo - Iván Todorovich
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-_logger = logging.getLogger(__name__)
+from openupgradelib import openupgrade
 
 
-def migrate(cr, version):
-
-    _logger.debug('Migrating account.account.type analytic_policy')
-
-    if not tools.column_exists(cr, 'account_account_type', 'analytic_policy'):
-        return
-
-    cr.execute("SELECT id FROM res_company")
-    company_ids = [d[0] for d in cr.fetchall()]
-
-    cr.execute(
-        "SELECT id FROM ir_model_fields WHERE model=%s AND name=%s",
-        ('account.account.type', 'property_analytic_policy'))
-    [field_id] = cr.fetchone()
-
-    for company_id in company_ids:
-        cr.execute("""
-            INSERT INTO ir_property (
-                name,
-                type,
-                fields_id,
-                company_id,
-                res_id,
-                value_text
-            )
-            SELECT
-                '{field}',
-                'selection',
-                {field_id},
-                {company_id},
-                CONCAT('{model},',id),
-                {oldfield}
-            FROM {table} t
-            WHERE t.{oldfield} IS NOT NULL
-            AND NOT EXISTS(
-                SELECT 1
-                FROM ir_property
-                WHERE fields_id={field_id}
-                AND company_id={company_id}
-                AND res_id=CONCAT('{model},',t.id)
-            )
-        """.format(
-            oldfield='analytic_policy',
-            field='property_analytic_policy',
-            field_id=field_id,
-            company_id=company_id,
-            model='account.account.type',
-            table='account_account_type',
-        ))
+@openupgrade.migrate()
+def migrate(env, version):
+    # Convert analytic_policy to property_analytic_policy
+    openupgrade.convert_to_company_dependent(
+        env=env,
+        model_name="account.account.type",
+        origin_field_name="analytic_policy",
+        destination_field_name="property_analytic_policy",
+    )
