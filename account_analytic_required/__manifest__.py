74bb84227287facdab927dc2bdd580eddc63219d
Iván Todorovich <ivan.todorovich@gmail.com>
2020-03-11 13:11:58 +0100

    [IMP] company_dependent analytic_policy

--- account_analytic_required/__manifest__.py
+++ account_analytic_required/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Account Analytic Required",
-    "version": "13.0.1.0.0",
+    "version": "13.0.2.0.0",
     "category": "Analytic Accounting",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
