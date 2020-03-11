ddeaf11eabcec84126bb1b88565b1b9829261f40
Iván Todorovich <ivan.todorovich@gmail.com>
2020-03-11 13:11:58 +0100

    [IMP] company_dependent analytic_policy

--- account_analytic_required/models/account.py
+++ account_analytic_required/models/account.py
@@ -1,5 +1,6 @@
 # Copyright 2011-2016 Akretion - Alexis de Lattre
 # Copyright 2016 Camptocamp SA
+# Copyright 2020 Druidoo - Iván Todorovich
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, api, exceptions, fields, models
@@ -9,7 +10,7 @@ from odoo.tools import float_is_zero
 class AccountAccountType(models.Model):
     _inherit = "account.account.type"
 
-    analytic_policy = fields.Selection(
+    property_analytic_policy = fields.Selection(
         selection=[
             ("optional", "Optional"),
             ("always", "Always"),
@@ -17,17 +18,22 @@ class AccountAccountType(models.Model):
             ("never", "Never"),
         ],
         string="Policy for analytic account",
-        required=True,
         default="optional",
-        help="Set the policy for analytic accounts : if you select "
-        "'Optional', the accountant is free to put an analytic account "
-        "on an account move line with this type of account ; if you "
-        "select 'Always', the accountant will get an error message if "
-        "there is no analytic account ; if you select 'Posted moves', "
-        "the accountant will get an error message if no analytic account "
-        "is defined when the move is posted ; if you select 'Never', "
-        "the accountant will get an error message if an analytic account "
-        "is present.",
+        company_dependent=True,
+        required=True,
+        help=(
+            "Sets the policy for analytic accounts.\n"
+            "If you select:\n"
+            "- Optional: The accountant is free to put an analytic account "
+            "on an account move line with this type of account.\n"
+            "- Always: The accountant will get an error message if "
+            "there is no analytic account.\n"
+            "- Posted moves: The accountant will get an error message if no "
+            "analytic account is defined when the move is posted.\n"
+            "- Never: The accountant will get an error message if an analytic "
+            "account is present.\n\n"
+            "This field is company dependent."
+        ),
     )
 
 
@@ -46,7 +52,9 @@ class AccountMoveLine(models.Model):
     @api.model
     def _get_analytic_policy(self, account):
         """ Extension point to obtain analytic policy for an account """
-        return account.user_type_id.analytic_policy
+        return account.user_type_id.with_context(
+            force_company=account.company_id.id
+        ).property_analytic_policy
 
     def _check_analytic_required_msg(self):
         for move_line in self:
