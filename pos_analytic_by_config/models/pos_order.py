038b5c98a8ffc54d1b1b28fa94c9b77df4aa115a
david <david.vidal@tecnativa.com>
2020-03-16 19:22:11 +0100

    [MIG] pos_analytic_by_config: Migration to 11.0

--- pos_analytic_by_config/models/pos_order.py
+++ pos_analytic_by_config/models/pos_order.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2015 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# Copyright 2015 ACSONE SA/NV
+# Copyright 2020 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import models, api
 
 
@@ -11,3 +10,7 @@ class PosOrder(models.Model):
     @api.model
     def _prepare_analytic_account(self, line):
         return line.order_id.session_id.config_id.account_analytic_id.id
+
+    def action_pos_order_invoice(self):
+        self_ctx = self.with_context(pos_analytic=True)
+        return super(PosOrder, self_ctx).action_pos_order_invoice()
