16e399e87108917b617523407c59a3bf6efd5330
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-03-21 19:25:46 +0100

    [IMP] account_analytic_sequence: assign on install

--- account_analytic_sequence/models/__init__.py
+++ account_analytic_sequence/models/__init__.py
@@ -1,3 +1,3 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import account_analytic_account
