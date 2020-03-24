91874946aa3bdb65c5163f2b26ea8f4bb18cb51e
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-24 18:58:37 +0700

    [MIG] procurement_mto_analytic: Migration to 13.0

--- procurement_mto_analytic/models/stock_move.py
+++ procurement_mto_analytic/models/stock_move.py
@@ -7,7 +7,7 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _prepare_procurement_values(self):
-        res = super(StockMove, self)._prepare_procurement_values()
+        res = super()._prepare_procurement_values()
         res.update(
             {"account_analytic_id": self.group_id.sale_id.analytic_account_id.id}
         )
