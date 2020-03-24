492c22cb7f2e99da07e128a9e7fcfa8befcc8ab3
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-03-24 08:26:00 +0100

    [MIG] stock_analytic: Migration to 13.0

--- stock_analytic/models/stock.py
+++ stock_analytic/models/stock.py
@@ -5,7 +5,7 @@
 # Copyright 2018 Hibou Corp.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
@@ -15,13 +15,12 @@ class StockMove(models.Model):
         string="Analytic Account", comodel_name="account.analytic.account",
     )
 
-    @api.multi
     def _prepare_account_move_line(
-        self, qty, cost, credit_account_id, debit_account_id
+        self, qty, cost, credit_account_id, debit_account_id, description
     ):
         self.ensure_one()
         res = super(StockMove, self)._prepare_account_move_line(
-            qty, cost, credit_account_id, debit_account_id
+            qty, cost, credit_account_id, debit_account_id, description
         )
         # Add analytic account in debit line
         if not self.analytic_account_id or not res:
@@ -32,9 +31,7 @@ class StockMove(models.Model):
                 res[num][2]["account_id"]
                 != self.product_id.categ_id.property_stock_valuation_account_id.id
             ):
-                res[num][2].update(
-                    {"analytic_account_id": self.analytic_account_id.id,}
-                )
+                res[num][2].update({"analytic_account_id": self.analytic_account_id.id})
         return res
 
 
