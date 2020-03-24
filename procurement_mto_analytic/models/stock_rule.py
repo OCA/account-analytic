fb957218633379a94d66432d7c43d87dee2c6ad7
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-24 18:58:37 +0700

    [MIG] procurement_mto_analytic: Migration to 13.0

--- procurement_mto_analytic/models/stock_rule.py
+++ procurement_mto_analytic/models/stock_rule.py
@@ -1,23 +1,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class StockRule(models.Model):
     _inherit = "stock.rule"
 
-    @api.multi
     def _prepare_purchase_order_line(
-        self, product_id, product_qty, product_uom, values, po, supplier
+        self, product_id, product_qty, product_uom, company_id, values, po
     ):
-        res = super(StockRule, self)._prepare_purchase_order_line(
-            product_id, product_qty, product_uom, values, po, supplier
+        res = super()._prepare_purchase_order_line(
+            product_id, product_qty, product_uom, company_id, values, po
         )
         res.update({"account_analytic_id": values.get("account_analytic_id", False)})
         return res
 
-    def _make_po_get_domain(self, values, partner):
-        res = super(StockRule, self)._make_po_get_domain(values, partner)
+    def _make_po_get_domain(self, company_id, values, partner):
+        res = super()._make_po_get_domain(company_id, values, partner)
         res += (
             (
                 "order_line.account_analytic_id",
