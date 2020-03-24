589a798bd1e675ea0870130fa43f1b7cf2258bc2
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-03-24 08:19:14 +0100

    [IMP] stock_analytic: black, isort

--- stock_analytic/__manifest__.py
+++ stock_analytic/__manifest__.py
@@ -9,19 +9,13 @@
     "summary": "Adds an analytic account in stock move",
     "version": "12.0.1.1.1",
     "author": "Julius Network Solutions, "
-              "ClearCorp, OpenSynergy Indonesia, "
-              "Hibou Corp., "
-              "Odoo Community Association (OCA)",
+    "ClearCorp, OpenSynergy Indonesia, "
+    "Hibou Corp., "
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
     "category": "Warehouse Management",
     "license": "AGPL-3",
-    "depends": [
-        "stock_account",
-        "analytic",
-    ],
-    "data": [
-        "views/stock_move_views.xml",
-        "views/stock_scrap.xml",
-    ],
-    'installable': True,
+    "depends": ["stock_account", "analytic",],
+    "data": ["views/stock_move_views.xml", "views/stock_scrap.xml",],
+    "installable": True,
 }
