1a54a6df8164a2ac02f732798edcbb98f66d15b3
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-03-24 08:19:14 +0100

    [IMP] stock_analytic: black, isort

--- stock_analytic/models/stock.py
+++ stock_analytic/models/stock.py
@@ -5,38 +5,40 @@
 # Copyright 2018 Hibou Corp.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     analytic_account_id = fields.Many2one(
-        string='Analytic Account',
-        comodel_name='account.analytic.account',
+        string="Analytic Account", comodel_name="account.analytic.account",
     )
 
     @api.multi
-    def _prepare_account_move_line(self, qty, cost,
-                                   credit_account_id, debit_account_id):
+    def _prepare_account_move_line(
+        self, qty, cost, credit_account_id, debit_account_id
+    ):
         self.ensure_one()
         res = super(StockMove, self)._prepare_account_move_line(
-            qty, cost, credit_account_id, debit_account_id)
+            qty, cost, credit_account_id, debit_account_id
+        )
         # Add analytic account in debit line
         if not self.analytic_account_id or not res:
             return res
 
         for num in range(0, 2):
-            if res[num][2]["account_id"] != self.product_id.\
-                    categ_id.property_stock_valuation_account_id.id:
-                res[num][2].update({
-                    'analytic_account_id': self.analytic_account_id.id,
-                })
+            if (
+                res[num][2]["account_id"]
+                != self.product_id.categ_id.property_stock_valuation_account_id.id
+            ):
+                res[num][2].update(
+                    {"analytic_account_id": self.analytic_account_id.id,}
+                )
         return res
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    analytic_account_id = fields.Many2one(
-        related='move_id.analytic_account_id')
+    analytic_account_id = fields.Many2one(related="move_id.analytic_account_id")
--- stock_analytic/models/stock_scrap.py
+++ stock_analytic/models/stock_scrap.py
@@ -7,13 +7,13 @@ class StockScrap(models.Model):
     _inherit = "stock.scrap"
 
     analytic_account_id = fields.Many2one(
-        string='Analytic Account',
-        comodel_name='account.analytic.account')
+        string="Analytic Account", comodel_name="account.analytic.account"
+    )
 
     @api.multi
     def _prepare_move_values(self):
         res = super()._prepare_move_values()
-        res.update({
-            'analytic_account_id': self.analytic_account_id.id,
-        })
+        res.update(
+            {"analytic_account_id": self.analytic_account_id.id,}
+        )
         return res
