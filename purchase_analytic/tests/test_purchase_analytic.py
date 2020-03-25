aaf4625c989ce696c05e3c13d5c27e6add1aca56
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-25 11:15:46 +0700

    [MIG] purchase_analytic: Migration to 13.0

--- purchase_analytic/tests/test_purchase_analytic.py
+++ purchase_analytic/tests/test_purchase_analytic.py
@@ -8,29 +8,31 @@ from odoo.tests.common import TransactionCase
 
 class TestPurchaseAnalytic(TransactionCase):
     def setUp(self):
-        super(TestPurchaseAnalytic, self).setUp()
+        super().setUp()
+        self.partner_id = self.env.ref("base.res_partner_12")
+        self.product_id = self.env.ref("product.product_product_9")
+        self.uom_id = self.env.ref("uom.product_uom_unit")
         self.project = self.env["account.analytic.account"].create(
             {"name": "Account Analytic for Tests"}
         )
 
     def test_analytic_account(self):
-        """ Create a purchase order with line
+        """ Create a purchase order (create)
             Set analytic account on purchase
             Check analytic account on line is set
         """
-        product_id = self.env.ref("product.product_product_9")
         po = self.env["purchase.order"].create(
             {
-                "partner_id": self.env.ref("base.res_partner_12").id,
+                "partner_id": self.partner_id.id,
                 "order_line": [
                     (
                         0,
                         0,
                         {
-                            "name": product_id.name,
-                            "product_id": product_id.id,
+                            "name": self.product_id.name,
+                            "product_id": self.product_id.id,
                             "product_qty": 1.0,
-                            "product_uom": self.env.ref("uom.product_uom_unit").id,
+                            "product_uom": self.uom_id.id,
                             "price_unit": 121.0,
                             "date_planned": datetime.today(),
                         },
@@ -38,21 +40,36 @@ class TestPurchaseAnalytic(TransactionCase):
                 ],
             }
         )
-
         po.project_id = self.project.id
+        po._onchange_project_id()
         self.assertEqual(po.project_id.id, self.project.id)
         self.assertEqual(po.order_line.account_analytic_id.id, self.project.id)
 
     def test_project_id(self):
-        """ Create a purchase order without line
+        """ Create a purchase order (new)
             Set analytic account on purchase
             Check analytic account is on purchase
         """
         po = self.env["purchase.order"].new(
             {
-                "partner_id": self.env.ref("base.res_partner_12").id,
+                "partner_id": self.partner_id.id,
                 "project_id": self.project.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": self.product_id.name,
+                            "product_id": self.product_id.id,
+                            "product_qty": 1.0,
+                            "product_uom": self.uom_id.id,
+                            "price_unit": 121.0,
+                            "date_planned": datetime.today(),
+                        },
+                    )
+                ],
             }
         )
         po._onchange_project_id()
         self.assertEqual(po.project_id.id, self.project.id)
+        self.assertFalse(po.order_line.account_analytic_id)
