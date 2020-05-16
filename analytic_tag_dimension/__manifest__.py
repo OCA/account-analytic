8fbe382a3810f6fa43e61e32c5d13013389ed5d8
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-05-17 01:23:20 +0200

    [IMP] analytic_tag_dimension: Sync dimension code and description changes

--- analytic_tag_dimension/__manifest__.py
+++ analytic_tag_dimension/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2017 PESOL (http://pesol.es)
 #                Angel Moya (angel.moya@pesol.es)
+# Copyright 2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Analytic Accounts Dimensions",
     'summary': "Group Analytic Entries by Dimensions",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.2.0",
     "license": "AGPL-3",
     "author": "PESOL, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
