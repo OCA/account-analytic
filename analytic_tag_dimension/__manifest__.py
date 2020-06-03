691bec12382459a66fabc5d8e920335cee1ed757
Saran440 <saranl@ecosoft.co.th>
2020-06-03 10:48:55 +0700

    [13.0][MIG] analytic_tag_dimension

--- analytic_tag_dimension/__manifest__.py
+++ analytic_tag_dimension/__manifest__.py
@@ -4,12 +4,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Analytic Accounts Dimensions",
-    'summary': "Group Analytic Entries by Dimensions",
-    "version": "12.0.1.2.1",
+    "summary": "Group Analytic Entries by Dimensions",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "PESOL, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
     "depends": ["analytic", "account"],
-    "data": ["views/analytic_view.xml", "security/ir.model.access.csv"],
+    "data": ["security/ir.model.access.csv", "views/analytic_view.xml"],
     "demo": ["demo/analytic_demo.xml"],
 }
