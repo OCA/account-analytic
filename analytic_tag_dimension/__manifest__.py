b99a4387a98bd6eced333d6f099331bd7b9a2b49
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-18 12:22:24 +0200

    [FIX] analytic_tag_dimension: avoid column not exists
    Creating dimensions, you can face this error:
    
    psycopg2.ProgrammingError: column ail.x_dimension_xxx does not exist
    LINE 48:         , ail.x_dimension_xxx as x_dimension_xxx, ai.team_..
    
    This is because the column is not yet populated in the moment when
    the model initialization is triggered.
    
    In this commit, we inhibit the population of that field in the report
    on that case, and launching manual initialization later when done.

--- analytic_tag_dimension/__manifest__.py
+++ analytic_tag_dimension/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Analytic Accounts Dimensions",
     "summary": "Group Analytic Entries by Dimensions",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
     "author": "PESOL, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
