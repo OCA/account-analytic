34973a0a8e40a9a37774ce464cf7c620bad1bf03
OCA Transbot <transbot@odoo-community.org>
2020-07-16 13:03:01 +0000

    [MIG] mrp_analytic: Migration to 13.0
    [UPD] Update mrp_analytic.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-analytic-13.0/account-analytic-13.0-mrp_analytic
    Translate-URL: https://translation.odoo-community.org/projects/account-analytic-13-0/account-analytic-13-0-mrp_analytic/

--- mrp_analytic/__manifest__.py
+++ mrp_analytic/__manifest__.py
@@ -1,5 +1,5 @@
-# © 2015 Eficent - Jordi Ballester Alomar
-# © 2015 Pedro M. Baeza - Antiun Ingeniería
+# Copyright 2015 Eficent - Jordi Ballester Alomar
+# Copyright 2015 Pedro M. Baeza - Antiun Ingeniería
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
