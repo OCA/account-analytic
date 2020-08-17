f0b38a9bd348ea53534952ad8b449d227880581b
david <david.vidal@tecnativa.com>
2020-08-17 07:30:34 +0200

    [MIG] pos_analytic_by_config: Migration to 12.0

--- pos_analytic_by_config/__manifest__.py
+++ pos_analytic_by_config/__manifest__.py
@@ -1,14 +1,16 @@
 # Copyright 2015 ACSONE SA/NV
+# Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "POS Analytic Config",
     'summary': """Use analytic account defined on
                   POS configuration for POS orders""",
     'author': 'ACSONE SA/NV,'
+              'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': "https://github.com/OCA/account-analytic",
     'category': 'Point Of Sale, Accounting',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'point_of_sale',
