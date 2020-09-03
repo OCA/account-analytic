7faae4c6970958c651677316de55bed9c217c82d
David Alonso (Solvos) <david.alonso@solvos.es>
2020-09-03 13:01:47 +0200

    [IMP] account_analytic_line_name_text: black, isort, prettier

--- account_analytic_line_name_text/__manifest__.py
+++ account_analytic_line_name_text/__manifest__.py
@@ -1,16 +1,14 @@
-# Copyright 2019 Solvos Consultor??a Inform??tica (<http://www.solvos.es>)
+# Copyright 2019 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Account Analytic Line Name Text',
-    'summary': """
+    "name": "Account Analytic Line Name Text",
+    "summary": """
         Changes account analytic line name field to Text.""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Analytic Accounting',
-    'author': "Odoo Community Association (OCA), Solvos",
-    'website': 'https://www.github.com/OCA/account-analytic',
-    'depends': [
-        'analytic',
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Analytic Accounting",
+    "author": "Odoo Community Association (OCA), Solvos",
+    "website": "https://www.github.com/OCA/account-analytic",
+    "depends": ["analytic"],
 }
