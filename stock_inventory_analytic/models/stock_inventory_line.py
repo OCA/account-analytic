40e7b18988c8c21516437430de3fafc43c336d64
Benoit Aimont <benoit.aimont@acsone.eu>
2020-11-03 10:11:52 +0100

    [IMP] stock_inventory_analytic: black, isort, prettier

--- stock_inventory_analytic/models/stock_inventory_line.py
+++ stock_inventory_analytic/models/stock_inventory_line.py
@@ -6,12 +6,14 @@ from odoo import fields, models
 class StockInventoryLine(models.Model):
     _inherit = "stock.inventory.line"
 
-    analytic_account_id = fields.Many2one('account.analytic.account',
-                                          string='Analytic Account')
+    analytic_account_id = fields.Many2one(
+        "account.analytic.account", string="Analytic Account"
+    )
 
     def _get_move_values(self, qty, location_id, location_dest_id, out):
         res = super(StockInventoryLine, self)._get_move_values(
-            qty, location_id, location_dest_id, out)
+            qty, location_id, location_dest_id, out
+        )
         if self.analytic_account_id:
-            res['analytic_account_id'] = self.analytic_account_id.id
+            res["analytic_account_id"] = self.analytic_account_id.id
         return res
