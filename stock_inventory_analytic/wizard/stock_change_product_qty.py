11490f943308db07a3a23400e2ba07d01f8d03f8
Benoit Aimont <benoit.aimont@acsone.eu>
2020-11-03 10:11:52 +0100

    [IMP] stock_inventory_analytic: black, isort, prettier

--- stock_inventory_analytic/wizard/stock_change_product_qty.py
+++ stock_inventory_analytic/wizard/stock_change_product_qty.py
@@ -4,13 +4,14 @@ from odoo import fields, models
 
 
 class StockChangeProductQty(models.TransientModel):
-    _inherit = 'stock.change.product.qty'
+    _inherit = "stock.change.product.qty"
 
-    analytic_account_id = fields.Many2one('account.analytic.account',
-                                          string='Analytic Account')
+    analytic_account_id = fields.Many2one(
+        "account.analytic.account", string="Analytic Account"
+    )
 
     def _action_start_line(self):
         res = super(StockChangeProductQty, self)._action_start_line()
         if self.analytic_account_id:
-            res.update({'analytic_account_id': self.analytic_account_id.id})
+            res.update({"analytic_account_id": self.analytic_account_id.id})
         return res
