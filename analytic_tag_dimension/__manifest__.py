7887bce5c75681619ff24a5ac8660d0d324a2f34
Saran440 <saranl@ecosoft.co.th>
2020-11-16 16:29:36 +0700

    [14.0][MIG] analytic_tag_dimension

--- analytic_tag_dimension/__manifest__.py
+++ analytic_tag_dimension/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Analytic Accounts Dimensions",
     "summary": "Group Analytic Entries by Dimensions",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "PESOL, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
