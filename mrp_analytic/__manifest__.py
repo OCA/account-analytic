2201187a07b8db94786dfa04744060090410639f
Daniel Reis <dreis@opensourceintegrators.com>
2020-12-10 20:29:51 +0000

    [MIG] mrp_analytic: black, isort, prettier

--- mrp_analytic/__manifest__.py
+++ mrp_analytic/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Analytic for manufacturing",
     "summary": "Adds the analytic account to the production order",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Manufacturing",
     "author": "ForgeFlow, "
     "Antiun Ingeniería, "
