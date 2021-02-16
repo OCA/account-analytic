fe4afb7b1bfe91384caeb9acd31cfee8bf7a6866
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2021-02-16 18:21:36 +0530

    [NEW] mrp_analytic_sale_project module added in the repo.

---
+++ mrp_analytic_sale_project/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Carry Analytic Account from Sales Order to Manufacturing Orders",
+    "summary": "Carry the Sales Order Analytic Account to "
+    "the generated Manufacturing Orders",
+    "version": "14.0.1.0.0",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Manufacturing",
+    "website": "https://github.com/OCA/account-analytic",
+    "license": "AGPL-3",
+    "depends": ["mrp_analytic", "sale_project"],
+    "installable": True,
+    "maintainer": "dreispt",
+    "development_status": "Beta",
+}
---
+++ mrp_analytic_sale_project/readme/USAGE.rst
@@ -0,0 +1,7 @@
+Salesperson confirms a Quotation into a Sales order:
+
+- A new Project is automatically created, along with the corresponding Analytic Account.
+
+- Manufacturing Orders are automatically created for the quoted products that are configured as "manufacturing to order" (MTO).
+
+- The Manufacturing Orders created from the Sales Order will automatically be assigned the corresponding Analytic Account. No user action is needed.
