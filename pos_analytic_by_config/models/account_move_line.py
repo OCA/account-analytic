f61efd11582291c64f10c4019190f09f4c260fee
david <david.vidal@tecnativa.com>
2021-04-05 12:54:00 +0200

    [MIG] pos_analytic_by_config: Migration to 13.0

--- pos_analytic_by_config/models/account_move_line.py
+++ pos_analytic_by_config/models/account_move_line.py
@@ -3,17 +3,17 @@
 from odoo import api, models
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
     @api.onchange("product_id")
     def _onchange_product_id(self):
-        account_analytic_id = self.account_analytic_id
+        analytic_account_id = self.analytic_account_id
         res = super()._onchange_product_id()
-        if not self.env.context.get("pos_analytic") or not account_analytic_id:
+        if not self.env.context.get("pos_analytic") or not analytic_account_id:
             return res
         # Odoo triggers an onchange on the product_id when creating an invoice.
         # This may cause an incompatibility with product_analytic
-        if self.account_analytic_id != account_analytic_id:
-            self.account_analytic_id = account_analytic_id
+        if self.analytic_account_id != analytic_account_id:
+            self.analytic_account_id = analytic_account_id
         return res
