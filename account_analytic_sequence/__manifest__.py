55a21d986061f11db419a3173679231792692629
Pierre Verkest <pierreverkest84@gmail.com>
2021-05-28 11:28:27 +0200

    [IMP] account_analytic_sequence: black, isort, prettier

--- account_analytic_sequence/__manifest__.py
+++ account_analytic_sequence/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Analytic Sequence',
-    'summary': """
+    "name": "Account Analytic Sequence",
+    "summary": """
         Restore the analytic account sequence""",
-    'version': '12.0.1.1.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-analytic',
-    'depends': [
-        'analytic',
+    "version": "12.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "analytic",
     ],
-    'data': [
-        'data/sequence.xml',
+    "data": [
+        "data/sequence.xml",
     ],
-    'post_init_hook': 'post_init_hook',
+    "post_init_hook": "post_init_hook",
 }
