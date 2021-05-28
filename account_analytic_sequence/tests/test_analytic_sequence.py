9f63bc25357d72660b5df3cdb999fe200fd8dcac
Pierre Verkest <pierreverkest84@gmail.com>
2021-05-28 11:28:27 +0200

    [IMP] account_analytic_sequence: black, isort, prettier

--- account_analytic_sequence/tests/test_analytic_sequence.py
+++ account_analytic_sequence/tests/test_analytic_sequence.py
@@ -6,15 +6,14 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountAnalyticSequence(TransactionCase):
-
     def setUp(self):
         super().setUp()
 
-        self.analytic_account_obj = self.env['account.analytic.account']
-        self.partner1 = self.env.ref('base.res_partner_1')
+        self.analytic_account_obj = self.env["account.analytic.account"]
+        self.partner1 = self.env.ref("base.res_partner_1")
         self.analytic = self.analytic_account_obj.create(
-            {'name': 'aa',
-             'partner_id': self.partner1.id})
+            {"name": "aa", "partner_id": self.partner1.id}
+        )
 
     def test_onchange(self):
         self.assertTrue(self.analytic.code, "Sequence not added")
