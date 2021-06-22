cac1d4ba8670964bb53076b47dea6f6f1a5dda76
Daniel Reis <dreis@opensourceintegrators.com>
2021-06-22 16:29:47 +0100

    [REF] account_analytic_wip: merge analytic_activity_based_cost feature to simplify dependencies

--- analytic_activity_based_cost/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2020 Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import activity_cost_rule
-from . import analytic_account
-from . import product_template
