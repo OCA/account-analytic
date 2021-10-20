ddac852032da5dc88d810477a0f7eb33ee780acf
Denis Roussel <denis.roussel@acsone.eu>
2021-10-20 09:26:36 +0200

    [14.0][IMP] stock_inventory_analytic: Improve tests

--- stock_inventory_analytic/tests/test_stock_inventory_analytic.py
+++ stock_inventory_analytic/tests/test_stock_inventory_analytic.py
@@ -2,86 +2,88 @@
 # Copyright 2019 brain-tec AG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestInventoryAnalytic(TransactionCase):
-    def setUp(self):
-        super(TestInventoryAnalytic, self).setUp()
+class TestInventoryAnalytic(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
 
         # MODELS
-        self.product_product_model = self.env["product.product"]
-        self.product_category_model = self.env["product.category"]
-        self.account_move_line_model = self.env["account.move.line"]
-        self.account_analytic_tag = self.env["account.analytic.tag"]
+        cls.product_product_model = cls.env["product.product"]
+        cls.product_category_model = cls.env["product.category"]
+        cls.account_move_line_model = cls.env["account.move.line"]
+        cls.account_analytic_tag = cls.env["account.analytic.tag"]
 
         # INSTANCES
-        self.analytic_tag_1 = self.account_analytic_tag.create(
+        cls.analytic_tag_1 = cls.account_analytic_tag.create(
             {"name": "Analytic Tag 1 (test)"}
         )
-        self.analytic_tag_2 = self.account_analytic_tag.create(
+        cls.analytic_tag_2 = cls.account_analytic_tag.create(
             {"name": "Analytic Tag 1 (test)"}
         )
-        self.stock_journal = self.env["account.journal"].create(
+        cls.stock_journal = cls.env["account.journal"].create(
             {"name": "Stock Journal", "code": "STJTEST", "type": "general"}
         )
-        self.valuation_account = self.env["account.account"].create(
+        cls.valuation_account = cls.env["account.account"].create(
             {
                 "name": "Test stock valuation",
                 "code": "tv",
-                "user_type_id": self.env["account.account.type"].search([], limit=1).id,
+                "user_type_id": cls.env["account.account.type"].search([], limit=1).id,
                 "reconcile": True,
-                "company_id": self.env.ref("base.main_company").id,
+                "company_id": cls.env.ref("base.main_company").id,
             }
         )
-        self.stock_input_account = self.env["account.account"].create(
+        cls.stock_input_account = cls.env["account.account"].create(
             {
                 "name": "Test stock input",
                 "code": "tsti",
-                "user_type_id": self.env["account.account.type"].search([], limit=1).id,
+                "user_type_id": cls.env["account.account.type"].search([], limit=1).id,
                 "reconcile": True,
-                "company_id": self.env.ref("base.main_company").id,
+                "company_id": cls.env.ref("base.main_company").id,
             }
         )
-        self.stock_output_account = self.env["account.account"].create(
+        cls.stock_output_account = cls.env["account.account"].create(
             {
                 "name": "Test stock output",
                 "code": "tout",
-                "user_type_id": self.env["account.account.type"].search([], limit=1).id,
+                "user_type_id": cls.env["account.account.type"].search([], limit=1).id,
                 "reconcile": True,
-                "company_id": self.env.ref("base.main_company").id,
+                "company_id": cls.env.ref("base.main_company").id,
             }
         )
-        self.category = self.product_category_model.create(
+        cls.category = cls.product_category_model.create(
             {
                 "name": "Physical (test)",
                 "property_cost_method": "standard",
                 "property_valuation": "real_time",
-                "property_stock_valuation_account_id": self.valuation_account.id,
-                "property_stock_account_input_categ_id": self.stock_input_account.id,
-                "property_stock_account_output_categ_id": self.stock_output_account.id,
-                "property_stock_journal": self.stock_journal.id,
+                "property_stock_valuation_account_id": cls.valuation_account.id,
+                "property_stock_account_input_categ_id": cls.stock_input_account.id,
+                "property_stock_account_output_categ_id": cls.stock_output_account.id,
+                "property_stock_journal": cls.stock_journal.id,
             }
         )
-        self.product = self.product_product_model.create(
+        cls.product = cls.product_product_model.create(
             {
                 "name": "Product (test)",
                 "type": "product",
-                "categ_id": self.category.id,
+                "categ_id": cls.category.id,
                 "price": 500,
                 "standard_price": 1000,
             }
         )
-        self.stock_location = self.env.ref("stock.stock_location_stock")
-        self.analytic_account = self.env.ref("analytic.analytic_agrolait")
-        self.company = self.env.user.company_id
+        cls.stock_location = cls.env.ref("stock.stock_location_stock")
+        cls.analytic_account = cls.env.ref("analytic.analytic_agrolait")
+        cls.company = cls.env.user.company_id
 
         # CONFIG
-        self.company.write(
+        cls.company.write(
             {
-                "analytic_account_id": self.analytic_account.id,
+                "analytic_account_id": cls.analytic_account.id,
                 "analytic_tag_ids": [
-                    (6, 0, [self.analytic_tag_1.id, self.analytic_tag_2.id])
+                    (6, 0, [cls.analytic_tag_1.id, cls.analytic_tag_2.id])
                 ],
             }
         )
