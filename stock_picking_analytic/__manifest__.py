cb9b2eb82ebf9d124ca5d68b9186eb59337736c1
Denis Roussel <denis.roussel@acsone.eu>
2021-10-20 19:09:47 +0200

    [14.0][ADD] stock_picking_analytic

---
+++ stock_picking_analytic/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Analytic",
+    "summary": """
+        Allows to define the analytic account on picking level""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": ["stock_analytic"],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+}
