64aa13902878ec9fac3e2815163beb57da463f1e
Denis Roussel <denis.roussel@acsone.eu>
2021-10-21 09:40:00 +0200

    [14.0][IMP] stock_inventory_analytic: Eficent => ForgeFlow

--- stock_inventory_analytic/__manifest__.py
+++ stock_inventory_analytic/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Stock Inventory Analytic",
     "summary": """
         Stock Inventory Analytic """,
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
     "category": "Warehouse Management",
     "version": "14.0.1.0.0",
