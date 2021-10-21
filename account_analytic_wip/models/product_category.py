27d6ad7b98eb1d7db94b5c65612802c808772f02
Daniel Reis <dreis.pt@hotmail.com>
2021-10-21 19:32:23 +0100

    [WIP][FIX] account_analytic_wip: fix clear WIP logic

--- account_analytic_wip/models/product_category.py
+++ account_analytic_wip/models/product_category.py
@@ -8,14 +8,14 @@ from odoo import fields, models
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
-    # property_wip_account_id = fields.Many2one(
-    #     "account.account",
-    #     "WIP Account",
-    #     company_dependent=True,
-    #     domain="[('company_id', '=', allowed_company_ids[0]), "
-    #     "('deprecated', '=', False)]",
-    #     check_company=True,
-    # )
+    property_wip_account_id = fields.Many2one(
+        "account.account",
+        "WIP Account",
+        company_dependent=True,
+        domain="[('company_id', '=', allowed_company_ids[0]), "
+        "('deprecated', '=', False)]",
+        check_company=True,
+    )
     property_variance_account_id = fields.Many2one(
         "account.account",
         "Variance Account",
