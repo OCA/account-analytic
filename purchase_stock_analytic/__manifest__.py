75e0d1a983300bcdf8b704732d8dd6bed8cc6130
Denis Roussel <denis.roussel@acsone.eu>
2021-11-02 10:39:15 +0100

    [14.0][MIG] purchase_stock_analytic

--- purchase_stock_analytic/__manifest__.py
+++ purchase_stock_analytic/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Purchase Stock Analytic",
     "summary": """
         Copies the analytic account of the purchase order item to the stock move""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
