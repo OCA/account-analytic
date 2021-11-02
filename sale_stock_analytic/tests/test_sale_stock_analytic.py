1b80390127a562a497ad823ed18df5bdc50b1c73
Denis Roussel <denis.roussel@acsone.eu>
2021-11-02 12:14:48 +0100

    [IMP] sale_stock_analytic: Improve tests

--- sale_stock_analytic/tests/test_sale_stock_analytic.py
+++ sale_stock_analytic/tests/test_sale_stock_analytic.py
@@ -1,37 +1,38 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestSaleStockAnalytic(TransactionCase):
-    def setUp(self):
-        super(TestSaleStockAnalytic, self).setUp()
-        self.sale_order_model = self.env["sale.order"]
-        self.sale_order_line_model = self.env["sale.order.line"]
-        self.analytic_tag_model = self.env["account.analytic.tag"]
-        self.product_model = self.env["product.product"]
-        self.res_partner_model = self.env["res.partner"]
+class TestSaleStockAnalytic(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.sale_order_model = cls.env["sale.order"]
+        cls.sale_order_line_model = cls.env["sale.order.line"]
+        cls.analytic_tag_model = cls.env["account.analytic.tag"]
+        cls.product_model = cls.env["product.product"]
+        cls.res_partner_model = cls.env["res.partner"]
 
-        self.analytic_tag_1 = self.analytic_tag_model.create({"name": "Tag test 1"})
-        self.analytic_tag_2 = self.analytic_tag_model.create({"name": "Tag test 2"})
-        self.partner = self.res_partner_model.create({"name": "Partner test"})
-        self.product = self.product_model.create({"name": "Product test"})
-        self.analytic_account = self.env.ref("analytic.analytic_agrolait")
+        cls.analytic_tag_1 = cls.analytic_tag_model.create({"name": "Tag test 1"})
+        cls.analytic_tag_2 = cls.analytic_tag_model.create({"name": "Tag test 2"})
+        cls.partner = cls.res_partner_model.create({"name": "Partner test"})
+        cls.product = cls.product_model.create({"name": "Product test"})
+        cls.analytic_account = cls.env.ref("analytic.analytic_agrolait")
 
-        self.sale_order = self.sale_order_model.create(
+        cls.sale_order = cls.sale_order_model.create(
             {
-                "partner_id": self.partner.id,
-                "analytic_account_id": self.analytic_account.id,
+                "partner_id": cls.partner.id,
+                "analytic_account_id": cls.analytic_account.id,
             }
         )
-        self.sale_order_line = self.sale_order_line_model.create(
+        cls.sale_order_line = cls.sale_order_line_model.create(
             {
                 "name": "sale order line test",
-                "order_id": self.sale_order.id,
-                "product_id": self.product.id,
+                "order_id": cls.sale_order.id,
+                "product_id": cls.product.id,
                 "analytic_tag_ids": [
-                    (6, 0, [self.analytic_tag_1.id, self.analytic_tag_2.id])
+                    (6, 0, [cls.analytic_tag_1.id, cls.analytic_tag_2.id])
                 ],
             }
         )
