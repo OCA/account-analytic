c66a109502010dc4b2d4c53ad57e661392298916
Denis Roussel <denis.roussel@acsone.eu>
2021-12-18 19:09:09 +0100

    [14.0][ADD] stock_inventory_analytic_warehouse_default

---
+++ stock_inventory_analytic_warehouse_default/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Inventory Analytic Warehouse Default",
+    "summary": """
+        Allows to define the default analytic account for inventory from warehouse""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "stock_warehouse_analytic",
+        "stock_inventory_analytic",
+    ],
+}
---
+++ stock_inventory_analytic_warehouse_default/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to set default analytic account from warehouse
+configuration to inventory lines.
