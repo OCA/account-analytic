2702907b33752ae177d6ff801c24ce15ac462d98
Denis Roussel <denis.roussel@acsone.eu>
2021-12-21 19:17:38 +0100

    [14.0][IMP] stock_inventory_analytic_warehouse_default: Add default on create
    If multiple locations are set on inventory, the default location is not well
    taken. So, in create, take the location value directly.

--- stock_inventory_analytic_warehouse_default/models/stock_inventory_line.py
+++ stock_inventory_analytic_warehouse_default/models/stock_inventory_line.py
@@ -1,7 +1,7 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class StockInventoryLine(models.Model):
@@ -29,3 +29,20 @@ class StockInventoryLine(models.Model):
             if warehouse.account_analytic_tag_ids:
                 return warehouse.analytic_account_tag_ids
         return super()._get_default_analytic_tags()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """
+        Get the default analytic account if inventory is set on multiple
+        locations
+        """
+        for vals in vals_list:
+            location_id = vals.get("location_id")
+            analytic_id = vals.get("analytic_account_id")
+            if location_id and not analytic_id:
+                vals["analytic_account_id"] = (
+                    self.with_context(default_location_id=location_id)
+                    ._get_default_analytic_account()
+                    .id
+                )
+        return super().create(vals_list)
