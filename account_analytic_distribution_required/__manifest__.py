b09eda9917ae3e779f00f3d3e156f10fde65cac3
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-17 11:29:55 -0500

    [IMP] account_analytic_distribution_required: black, isort, prettier

--- account_analytic_distribution_required/__manifest__.py
+++ account_analytic_distribution_required/__manifest__.py
@@ -1,19 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Acsone - Stéphane Bidoul <stephane.bidoul@acsone.eu>
 # Copyright 2017 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account Analytic Distribution Required',
-    'version': '12.0.2.0.0',
-    'category': 'Analytic Accounting',
-    'license': 'AGPL-3',
-    'author': "ACSONE SA/NV, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
-    'website': 'http://www.acsone.eu/',
-    'depends': [
-        'account_analytic_required',
+    "name": "Account Analytic Distribution Required",
+    "version": "12.0.2.0.0",
+    "category": "Analytic Accounting",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "account_analytic_required",
     ],
-    'application': False,
-    'installable': True,
+    "application": False,
+    "installable": True,
 }
