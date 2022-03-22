1352f5f8946d6a4699615bf29bd8d6ed7ee75199
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-03-22 11:04:03 +0530

    [MIG] account_analytic_tag_default: Migration to 15.0

--- account_analytic_tag_default/__manifest__.py
+++ account_analytic_tag_default/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Account Analytic Tag Default",
     "summary": "Set default tags on analytic accounts.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
