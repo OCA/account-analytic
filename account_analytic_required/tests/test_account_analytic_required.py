fb5322d24ceced9e792bf989b25035afe01f92a2
Rod <rod.schouteden@dynapps.be>
2022-05-18 16:47:34 +0200

    [15.0][mig] account_analytic_required

--- account_analytic_required/tests/test_account_analytic_required.py
+++ account_analytic_required/tests/test_account_analytic_required.py
@@ -15,6 +15,14 @@ class TestAccountAnalyticRequired(common.TransactionCase):
         cls.move_line_obj = cls.env["account.move.line"]
         cls.analytic_account_obj = cls.env["account.analytic.account"]
         cls.analytic_account = cls.analytic_account_obj.create({"name": "test aa"})
+        cls.analytic_account_1 = cls.analytic_account_obj.create(
+            {"name": "test aa 1 for distribution"}
+        )
+        cls.analytic_account_2 = cls.analytic_account_obj.create(
+            {"name": "test aa 2 for distribution"}
+        )
+        cls.analytic_tag_obj = cls.env["account.analytic.tag"]
+        cls.analytic_distribution_obj = cls.env["account.analytic.distribution"]
         cls.account_sales = cls.account_obj.create(
             {
                 "code": "X1020",
@@ -44,9 +52,36 @@ class TestAccountAnalyticRequired(common.TransactionCase):
                 "type": "sale",
             }
         )
+        cls.analytic_tag = cls.analytic_tag_obj.create(
+            {
+                "name": "Analytic tag for test",
+                "company_id": cls.env.company.id,
+                "active_analytic_distribution": True,
+            }
+        )
+        cls.analytic_tag_wo_distribution = cls.analytic_tag_obj.create(
+            {"name": "Analytic tag for test 2", "company_id": cls.env.company.id}
+        )
+        cls.analytic_distribution_line_1 = cls.analytic_distribution_obj.create(
+            {
+                "name": "Analytic distribution line 1",
+                "account_id": cls.analytic_account_1.id,
+                "tag_id": cls.analytic_tag.id,
+                "percentage": 50.0,
+            }
+        )
+        cls.analytic_distribution_line_2 = cls.analytic_distribution_obj.create(
+            {
+                "name": "Analytic distribution line 2",
+                "account_id": cls.analytic_account_1.id,
+                "tag_id": cls.analytic_tag.id,
+                "percentage": 50.0,
+            }
+        )
 
     def _create_move(self, amount=100, **kwargs):
         with_analytic = kwargs.get("with_analytic")
+        with_analytic_tag = kwargs.get("with_analytic_tag")
         date = datetime.now()
         ml_obj = self.move_line_obj.with_context(check_move_validity=False)
         move_vals = {"name": "/", "journal_id": self.sales_journal.id, "date": date}
@@ -60,6 +95,11 @@ class TestAccountAnalyticRequired(common.TransactionCase):
                 "account_id": self.account_sales.id,
                 "analytic_account_id": self.analytic_account.id
                 if with_analytic
+                else False,
+                "analytic_tag_ids": [
+                    (6, 0, [self.analytic_tag.id, self.analytic_tag_wo_distribution.id])
+                ]
+                if with_analytic_tag
                 else False,
             }
         )
@@ -148,3 +188,13 @@ class TestAccountAnalyticRequired(common.TransactionCase):
         move = line.move_id
         move.action_post()
         self.assertEqual(move.state, "posted")
+
+    def test_exception_policy_for_analytic_distribution(self):
+        self._set_analytic_policy("always")
+        self._create_move(with_analytic=False, with_analytic_tag=True)
+        self._set_analytic_policy("posted")
+        line = self._create_move(with_analytic=False, with_analytic_tag=True)
+        line.move_id.post()
+        self._set_analytic_policy("never")
+        with self.assertRaises(exceptions.ValidationError), self.cr.savepoint():
+            self._create_move(with_analytic=False, with_analytic_tag=True)
