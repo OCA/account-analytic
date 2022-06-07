d73a01c7b5848eef0af01d4a7d5593b947300ca4
Alexis de Lattre <alexis.delattre@akretion.com>
2022-06-07 18:16:15 +0200

    [ADD] product_analytic_sale
    The readme gives an example of a scenario where this module product_analytic_sale is required.

---
+++ product_analytic_sale/models/sale_order_line.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _prepare_invoice_line(self, **optional_values):
+        vals = super()._prepare_invoice_line(**optional_values)
+        ana_account = self.product_id.product_tmpl_id._get_product_analytic_accounts()[
+            "income"
+        ]
+        if ana_account:
+            vals["analytic_account_id"] = ana_account.id
+        return vals
