bafe5d046eaadf32e308bd17e7624bfdd8b00738
urvisha-serpentcs <urvisha.desai@serpentcs.com>
2022-08-16 16:21:18 +0530

    [ADD] migrate sale_stock_analytic module from v14 to v15

---
+++ sale_stock_analytic/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Stock Analytic",
+    "summary": """
+        Copies the analytic account of the sale order and the analytic tags
+        of the sale order line to the stock move""",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": ["sale_stock", "stock_analytic"],
+}
