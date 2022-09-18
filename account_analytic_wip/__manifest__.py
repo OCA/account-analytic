dda8f747948731675d7669948ca6ca7d6c6d4e6b
Fernando <fernando.lachica@greenice.com>
2022-09-18 16:17:48 +0200

    [MIG] account_analytic_wip 15.0

--- account_analytic_wip/__manifest__.py
+++ account_analytic_wip/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Analytic Accounting support for WIP and Variances",
-    "version": "14.0.3.0.2",
+    "version": "15.0.1.0.0",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "summary": "Track and report WIP and Variances based on Analytic Items",
     "website": "https://github.com/OCA/account-analytic",
--- account_analytic_wip/readme/CONTRIBUTORS.rst
+++ account_analytic_wip/readme/CONTRIBUTORS.rst
@@ -2,3 +2,7 @@
 
   * Daniel Reis <dreis@opensourceintegrators.com>
   * Chandresh Thakkar <cthakkar@opensourceintegrators.com>
+
+* `GreenIce <https://greenice.com>`:
+
+  * Fernando La Chica <fernandolachica@gmail.com>
