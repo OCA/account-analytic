b033c600525bd429d43ea812b5a67b8d8b5f300d
remi-filament <30716308+remi-filament@users.noreply.github.com>
2022-11-16 14:58:58 +0100

    [ADD] module account_move_update_analytic

---
+++ account_move_update_analytic/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Le Filament
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account move update analytic",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": "This module allows the user to update analytic on posted moves",
+    "author": "Le Filament, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/account_move_update_analytic_view.xml",
+        "views/account_move_view.xml",
+        "views/account_move_line_view.xml",
+    ],
+    "installable": True,
+}
