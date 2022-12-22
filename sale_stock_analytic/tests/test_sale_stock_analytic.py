2952ea3e5567adaa9a8a400ca801ff024c30c502
nicolas <nmr@adhoc.com.ar>
2022-12-22 11:08:01 -0300

    [MIG] sale_stock_analytic: Migration to 16.0

--- sale_stock_analytic/tests/test_sale_stock_analytic.py
+++ sale_stock_analytic/tests/test_sale_stock_analytic.py
@@ -10,20 +10,18 @@ class TestSaleStockAnalytic(SavepointCase):
         super().setUpClass()
         cls.sale_order_model = cls.env["sale.order"]
         cls.sale_order_line_model = cls.env["sale.order.line"]
-        cls.analytic_tag_model = cls.env["account.analytic.tag"]
         cls.product_model = cls.env["product.product"]
         cls.res_partner_model = cls.env["res.partner"]
 
-        cls.analytic_tag_1 = cls.analytic_tag_model.create({"name": "Tag test 1"})
-        cls.analytic_tag_2 = cls.analytic_tag_model.create({"name": "Tag test 2"})
         cls.partner = cls.res_partner_model.create({"name": "Partner test"})
         cls.product = cls.product_model.create({"name": "Product test"})
-        cls.analytic_account = cls.env.ref("analytic.analytic_agrolait")
+        cls.analytic_distribution = dict(
+            {str(cls.env.ref("analytic.analytic_agrolait").id): 100.0}
+        )
 
         cls.sale_order = cls.sale_order_model.create(
             {
                 "partner_id": cls.partner.id,
-                "analytic_account_id": cls.analytic_account.id,
             }
         )
         cls.sale_order_line = cls.sale_order_line_model.create(
@@ -31,17 +29,11 @@ class TestSaleStockAnalytic(SavepointCase):
                 "name": "sale order line test",
                 "order_id": cls.sale_order.id,
                 "product_id": cls.product.id,
-                "analytic_tag_ids": [
-                    (6, 0, [cls.analytic_tag_1.id, cls.analytic_tag_2.id])
-                ],
+                "analytic_distribution": cls.analytic_distribution,
             }
         )
 
     def test_sale_stock_analytic(self):
         self.sale_order.action_confirm()
         self.move = self.sale_order.picking_ids.move_ids_without_package
-        self.assertEqual(self.move.analytic_account_id, self.analytic_account)
-        self.assertEqual(
-            self.move.analytic_tag_ids.ids,
-            [self.analytic_tag_1.id, self.analytic_tag_2.id],
-        )
+        self.assertEqual(self.move.analytic_distribution, self.analytic_distribution)
