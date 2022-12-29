0a9c4a7a35d83b2c31f6b3359b775a4504c297c4
Ruchir Shukla <ruchir@bizzappdev.com>
2022-12-29 16:27:55 +0530

    [IMP]purchase_stock_analytic: black, isort, prettier

--- purchase_stock_analytic/__manifest__.py
+++ purchase_stock_analytic/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Purchase Stock Analytic",
     "summary": """
         Copies the analytic account of the purchase order item to the stock move""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
