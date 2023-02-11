f6e0b7fdd4135757323ac800c96f7f69eaa97a86
Yoshi Tashiro <tashiro@quartile.co>
2023-02-11 13:20:40 +0000

    [ADD] mrp_stock_analytic

---
+++ mrp_stock_analytic/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Stock Analytic",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "depends": ["mrp_account", "stock_analytic"],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
---
+++ mrp_stock_analytic/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+Allows to assign the analytic distribution in the manufacturing order, which should
+propagate the value to the corresponding field of the related component stock moves.
+
+This function can be useful when cost analysis needs to be done on flushed components.
+
+This module depends on OCA module stock_analytic.
