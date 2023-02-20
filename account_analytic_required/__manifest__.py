25cb9dc2b91a880916f22d555b566a523835a453
Minh Chien <amemberofuit@gmail.com>
2023-02-20 11:00:46 +0700

    [16.0][MIG] account_analytic_required: Migrate to version 16.0

--- account_analytic_required/__manifest__.py
+++ account_analytic_required/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2011-2020 Akretion - Alexis de Lattre
-# Copyright 2016-2020 Camptocamp SA
+# Copyright Akretion - Alexis de Lattre
+# Copyright Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Account Analytic Required",
-    "version": "15.0.1.0.2",
+    "version": "16.0.1.0.0",
     "category": "Analytic Accounting",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
     "depends": ["account_usability"],
-    "data": ["views/account_account_type.xml"],
+    "data": ["views/account_account_views.xml"],
     "installable": True,
 }
--- account_analytic_required/models/account.py
+++ account_analytic_required/models/account.py
@@ -6,10 +6,10 @@
 from odoo import _, api, exceptions, fields, models
 
 
-class AccountAccountType(models.Model):
-    _inherit = "account.account.type"
+class AccountAccount(models.Model):
+    _inherit = "account.account"
 
-    property_analytic_policy = fields.Selection(
+    analytic_policy = fields.Selection(
         selection=[
             ("optional", "Optional"),
             ("always", "Always"),
@@ -34,16 +34,10 @@ class AccountAccountType(models.Model):
         ),
     )
 
-
-class AccountAccount(models.Model):
-    _inherit = "account.account"
-
     def _get_analytic_policy(self):
         """Extension point to obtain analytic policy for an account"""
         self.ensure_one()
-        return self.user_type_id.with_company(
-            self.company_id.id
-        ).property_analytic_policy
+        return self.analytic_policy
 
 
 class AccountMove(models.Model):
@@ -58,25 +52,13 @@ class AccountMove(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    def _has_analytic_distribution(self):
-        # If the move line has an analytic tag with distribution, the field
-        # analytic_account_id may be empty. So in this case, we do not check it.
-        tags_with_analytic_distribution = self.analytic_tag_ids.filtered(
-            "active_analytic_distribution"
-        )
-        return bool(tags_with_analytic_distribution.analytic_distribution_ids)
-
     def _check_analytic_required_msg(self):
         self.ensure_one()
         company_cur = self.company_currency_id
         if company_cur.is_zero(self.debit) and company_cur.is_zero(self.credit):
             return None
         analytic_policy = self.account_id._get_analytic_policy()
-        if (
-            analytic_policy == "always"
-            and not self.analytic_account_id
-            and not self._has_analytic_distribution()
-        ):
+        if analytic_policy == "always" and not self.analytic_distribution:
             return _(
                 "Analytic policy is set to 'Always' with account "
                 "'%(account)s' but the analytic account is missing in "
@@ -85,12 +67,11 @@ class AccountMoveLine(models.Model):
                 "account": self.account_id.display_name,
                 "move": self.name or "",
             }
-        elif analytic_policy == "never" and (
-            self.analytic_account_id or self._has_analytic_distribution()
-        ):
-            analytic_account = (
-                self.analytic_account_id
-                or self.analytic_tag_ids.analytic_distribution_ids[:1]
+        elif analytic_policy == "never" and (self.analytic_distribution):
+            analytic_account = self.analytic_distribution
+            analytic_acc_ids = [int(k) for k in analytic_account.keys()]
+            analytic_accs = self.env["account.analytic.account"].browse(
+                analytic_acc_ids
             )
             return _(
                 "Analytic policy is set to 'Never' with account "
@@ -99,13 +80,12 @@ class AccountMoveLine(models.Model):
             ) % {
                 "account": self.account_id.display_name,
                 "move": self.name or "",
-                "analytic_account": analytic_account.display_name,
+                "analytic_account": ", ".join(analytic_accs.mapped("name")),
             }
         elif (
             analytic_policy == "posted"
-            and not self.analytic_account_id
+            and not self.analytic_distribution
             and self.move_id.state == "posted"
-            and not self._has_analytic_distribution()
         ):
             return _(
                 "Analytic policy is set to 'Posted moves' with "
@@ -117,7 +97,7 @@ class AccountMoveLine(models.Model):
             }
         return None
 
-    @api.constrains("analytic_account_id", "account_id", "debit", "credit")
+    @api.constrains("analytic_distribution", "account_id", "debit", "credit")
     def _check_analytic_required(self):
         for rec in self:
             message = rec._check_analytic_required_msg()
