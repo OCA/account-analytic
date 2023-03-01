f24b05d2401bc9da5735d52c74ea9c43f1a1bd0f
Yoshi Tashiro <tashiro@quartile.co>
2023-03-01 01:36:50 +0000

    fixup

--- mrp_subcontracting_analytic/models/stock_picking.py
+++ mrp_subcontracting_analytic/models/stock_picking.py
@@ -7,7 +7,13 @@ from odoo import models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def _prepare_subcontract_mo_vals(self, subcontract_move, bom):
-        vals = super()._prepare_subcontract_mo_vals(subcontract_move, bom)
-        vals["analytic_distribution"] = subcontract_move.analytic_distribution
-        return vals
+    # pylint: disable=W8110
+    def _subcontracted_produce(self, subcontract_details):
+        # extending _prepare_subcontract_mo_vals() to pass the analytic distribution to
+        # production would cause unexpected result (production moves get duplicated),
+        # therefore, instead, we trigger the inverse method upon creation of the
+        # subcontracted production.
+        super()._subcontracted_produce(subcontract_details)
+        self.ensure_one()
+        for move, _bom in subcontract_details:
+            move._inverse_analytic_distribution()
