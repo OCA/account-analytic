139febc29954cf950548a2715b460721068cb005
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-07 17:55:36 +0530

    [MIG][15.0]product_analytic_purchase:Migration to 15.0

--- product_analytic_purchase/__manifest__.py
+++ product_analytic_purchase/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Analytic Purchase",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.1",
     "category": "Purchases",
     "license": "AGPL-3",
     "summary": "Glue module between purchase and product_analytic",
