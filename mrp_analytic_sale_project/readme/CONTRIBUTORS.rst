f48d5c1cc5361734eaf3ee3a8b41fffc9ab31ceb
Raul-S73 <raul@studio73.es>
2023-03-22 15:54:32 +0100

    [MIG] mrp_analytic_sale_project: migration to 16.0

--- mrp_analytic_sale_project/readme/CONTRIBUTORS.rst
+++ mrp_analytic_sale_project/readme/CONTRIBUTORS.rst
@@ -2,3 +2,7 @@
 
     * Daniel Reis <dreis@opensourceintegrators.com>
     * Chandresh Thakkar <cthakkr@opensourceintegrators.com>
+
+* `Studio73 <https://www.studio73.es>`__:
+
+    * Raúl Menéndez <raul@studio73.es>
--- mrp_analytic_sale_project/tests/test_sale_analytic.py
+++ mrp_analytic_sale_project/tests/test_sale_analytic.py
@@ -2,29 +2,30 @@ from odoo.tests import common
 
 
 class TestSaleAnalytic(common.TransactionCase):
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        mfg_route = self.env.ref("mrp.route_warehouse0_manufacture")
-        mto_route = self.env.ref("stock.route_warehouse0_mto")
+        mfg_route = cls.env.ref("mrp.route_warehouse0_manufacture")
+        mto_route = cls.env.ref("stock.route_warehouse0_mto")
         mto_route.active = True
 
-        self.product_service = self.env["product.product"].create(
+        cls.product_service = cls.env["product.product"].create(
             {
                 "name": "Service",
                 "type": "service",
                 "service_tracking": "task_in_project",
             }
         )
-        self.product_stock = self.env["product.product"].create(
+        cls.product_stock = cls.env["product.product"].create(
             {
                 "name": "Storable",
                 "type": "product",
                 "route_ids": [(6, 0, [mfg_route.id, mto_route.id])],
             }
         )
-        self.bom_product_stock = self.env["mrp.bom"].create(
-            {"product_tmpl_id": self.product_stock.product_tmpl_id.id}
+        cls.bom_product_stock = cls.env["mrp.bom"].create(
+            {"product_tmpl_id": cls.product_stock.product_tmpl_id.id}
         )
 
     def test_100_sale_analytic_to_mo(self):
@@ -54,4 +55,4 @@ class TestSaleAnalytic(common.TransactionCase):
         mo = self.env["mrp.production"].search(
             [("analytic_account_id", "=", so_analytic.id)]
         )
-        self.assertEquals(len(mo), 1, "Expected one MO with the SO's Analytic Account")
+        self.assertEqual(len(mo), 1, "Expected one MO with the SO's Analytic Account")
