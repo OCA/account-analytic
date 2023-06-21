ed8ef6ff94c9b04b613093e50b82b1030b347974
A. Hochuli <andreas.hochuli@camptocamp.com>
2023-06-21 18:11:14 +0200

    [Fix] Unit test analytic account 6

--- pos_analytic_by_config/models/pos_config.py
+++ pos_analytic_by_config/models/pos_config.py
@@ -6,6 +6,6 @@ from odoo import fields, models
 class PosConfig(models.Model):
     _inherit = "pos.config"
 
-    pos_account_analytic_id = fields.Many2one(
+    account_analytic_id = fields.Many2one(
         comodel_name="account.analytic.account", string="Analytic Account"
     )
