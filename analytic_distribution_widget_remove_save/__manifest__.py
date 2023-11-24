2406d81a8c7a6720d9b3c3217b0142e5a7e5efa8
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2023-11-24 12:45:11 +0100

    [ADD][16.0] analytic_distribution_widget_remove_save: init

---
+++ analytic_distribution_widget_remove_save/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Analytic Distribution Widget Remove Save",
+    "summary": """
+        Remove save button on analytic distribution widget""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "analytic",
+    ],
+    "data": [],
+    "demo": [],
+    "assets": {
+        "web.assets_backend": [
+            "analytic_distribution_widget_remove_save/static/src/xml/**/*",
+        ]
+    },
+}
---
+++ analytic_distribution_widget_remove_save/readme/CONTEXT.md
@@ -0,0 +1,1 @@
+The 'Save' button serves as a shortcut for quickly creating analytic distribution templates, but it may confuse untrained users, potentially leading to the unintended creation of templates.
