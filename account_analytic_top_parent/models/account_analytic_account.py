25ad69b12d6834df994595890dba71d834eb1546
Victor Champonnois <victor@coopiteasy.be>
2023-12-07 11:56:11 +0100

    [ADD] account_analytic_top_parent

---
+++ account_analytic_top_parent/models/account_analytic_account.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from odoo import api, fields, models
+
+
+class AccountAnalytic(models.Model):
+    _inherit = 'account.analytic.account'
+
+    top_parent_id = fields.Many2one('account.analytic.account',
+                                   compute="_compute_top_parent_analytic_account",
+                                   string="Top Parent Analytic Account",
+                                   store=True)
+
+    def _get_top_parent_analytic_account(self):
+        self.ensure_one()
+        if self.parent_id:
+            return self.parent_id._get_top_parent_analytic_account()
+        else:
+            return self
+
+    @api.depends('parent_id', 'parent_id.top_parent_id')
+    def _compute_top_parent_analytic_account(self):
+        for account in self:
+            account.top_parent_id = account._get_top_parent_analytic_account()
---
+++ account_analytic_top_parent/tests/test_account_invoice.py
@@ -0,0 +1,32 @@
+# Copyright 2015 Antiun Ingenieria - Javier Iniesta
+# Copyright 2017 Tecnativa - Luis Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestAccountInvoiceLine(TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+        self.analytic_account_A = self.env['account.analytic.account'].create({
+            'name': 'test analytic_account_A'})
+        self.analytic_account_AB = self.env['account.analytic.account'].create(
+            {'name': 'test analytic_account_AB',
+             'parent_id': self.analytic_account_A.id,}
+            )
+        self.analytic_account_ABC = self.env['account.analytic.account'].create(
+            {'name': 'test analytic_account_AB',
+             'parent_id': self.analytic_account_AB.id,}
+            )
+
+
+    def test_top_parent_if_no_parent_returns_itself(self):
+        self.assertEqual(self.analytic_account_A.top_parent_id, 
+                         self.analytic_account_A.top_parent_id)
+
+    def test_top_parent_if_parent(self):
+        self.assertEqual(self.analytic_account_AB.top_parent_id, self.analytic_account_A)
+
+    def test_top_parent_if_grand_parent(self):
+        self.assertEqual(self.analytic_account_ABC.top_parent_id, self.analytic_account_A)
