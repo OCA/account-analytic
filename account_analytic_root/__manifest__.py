e4bfbf48c86b5f76719abb5af909bd83e1c7b1fc
Victor Champonnois <victor@coopiteasy.be>
2023-12-07 11:56:11 +0100

    [ADD] account_analytic_root

---
+++ account_analytic_root/__manifest__.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Root Analytic Account",
+    "summary": """Compute the Root Analytic Account""",
+    "version": "12.0.1.0.1",
+    "category": "Account",
+    "website": "https://github.com/OCA/account-analytic",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "maintainers": ["victor-champonnois"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "account_analytic_parent",
+    ],
+}
