1675bf4a9405f7bc56e982e90451a654c20155bd
Marcos Oitaben <marcos.oitaben@dynapps.eu>
2023-12-21 16:09:56 +0100

    [MIG] sale_stock_analytic: Migration to 15.0

--- sale_stock_analytic/tests/test_sale_stock_analytic.py
+++ sale_stock_analytic/tests/test_sale_stock_analytic.py
@@ -1,10 +1,10 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestSaleStockAnalytic(SavepointCase):
+class TestSaleStockAnalytic(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
