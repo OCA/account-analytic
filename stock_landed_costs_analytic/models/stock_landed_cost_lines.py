317da52176a47fa697021a45ef781c15009a2d5a
Rocío Vega <rov@adhoc.com.ar>
2024-01-04 08:59:59 -0300

    [IMP] stock_landed_costs_analytic: pre-commit execution

--- stock_landed_costs_analytic/models/stock_landed_cost_lines.py
+++ stock_landed_costs_analytic/models/stock_landed_cost_lines.py
@@ -9,6 +9,7 @@ class StockLandedCostLines(models.Model):
     _inherit = "stock.landed.cost.lines"
 
     analytic_account_id = fields.Many2one(
-        string="Analytic Account", comodel_name="account.analytic.account",
+        string="Analytic Account",
+        comodel_name="account.analytic.account",
     )
     analytic_tag_ids = fields.Many2many("account.analytic.tag", string="Analytic Tags")
