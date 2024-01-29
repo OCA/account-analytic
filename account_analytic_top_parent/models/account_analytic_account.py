9ad21cfa349ad7c86bac8c51501360f5bd8992f5
Victor Champonnois <victor@coopiteasy.be>
2024-01-29 09:58:27 +0100

    [IMP] account_analytic_top_parent: black, isort, prettier

--- account_analytic_top_parent/models/account_analytic_account.py
+++ account_analytic_top_parent/models/account_analytic_account.py
@@ -6,12 +6,14 @@ from odoo import api, fields, models
 
 
 class AccountAnalytic(models.Model):
-    _inherit = 'account.analytic.account'
+    _inherit = "account.analytic.account"
 
-    top_parent_id = fields.Many2one('account.analytic.account',
-                                   compute="_compute_top_parent_analytic_account",
-                                   string="Top Parent Analytic Account",
-                                   store=True)
+    top_parent_id = fields.Many2one(
+        "account.analytic.account",
+        compute="_compute_top_parent_analytic_account",
+        string="Top Parent Analytic Account",
+        store=True,
+    )
 
     def _get_top_parent_analytic_account(self):
         self.ensure_one()
@@ -20,7 +22,7 @@ class AccountAnalytic(models.Model):
         else:
             return self
 
-    @api.depends('parent_id', 'parent_id.top_parent_id')
+    @api.depends("parent_id", "parent_id.top_parent_id")
     def _compute_top_parent_analytic_account(self):
         for account in self:
             account.top_parent_id = account._get_top_parent_analytic_account()
--- account_analytic_top_parent/tests/test_account_invoice.py
+++ account_analytic_top_parent/tests/test_account_invoice.py
@@ -6,27 +6,35 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountInvoiceLine(TransactionCase):
-
     def setUp(self):
         super().setUp()
-        self.analytic_account_A = self.env['account.analytic.account'].create({
-            'name': 'test analytic_account_A'})
-        self.analytic_account_AB = self.env['account.analytic.account'].create(
-            {'name': 'test analytic_account_AB',
-             'parent_id': self.analytic_account_A.id,}
-            )
-        self.analytic_account_ABC = self.env['account.analytic.account'].create(
-            {'name': 'test analytic_account_AB',
-             'parent_id': self.analytic_account_AB.id,}
-            )
-
+        self.analytic_account_A = self.env["account.analytic.account"].create(
+            {"name": "test analytic_account_A"}
+        )
+        self.analytic_account_AB = self.env["account.analytic.account"].create(
+            {
+                "name": "test analytic_account_AB",
+                "parent_id": self.analytic_account_A.id,
+            }
+        )
+        self.analytic_account_ABC = self.env["account.analytic.account"].create(
+            {
+                "name": "test analytic_account_AB",
+                "parent_id": self.analytic_account_AB.id,
+            }
+        )
 
     def test_top_parent_if_no_parent_returns_itself(self):
-        self.assertEqual(self.analytic_account_A.top_parent_id, 
-                         self.analytic_account_A.top_parent_id)
+        self.assertEqual(
+            self.analytic_account_A.top_parent_id, self.analytic_account_A.top_parent_id
+        )
 
     def test_top_parent_if_parent(self):
-        self.assertEqual(self.analytic_account_AB.top_parent_id, self.analytic_account_A)
+        self.assertEqual(
+            self.analytic_account_AB.top_parent_id, self.analytic_account_A
+        )
 
     def test_top_parent_if_grand_parent(self):
-        self.assertEqual(self.analytic_account_ABC.top_parent_id, self.analytic_account_A)
+        self.assertEqual(
+            self.analytic_account_ABC.top_parent_id, self.analytic_account_A
+        )
