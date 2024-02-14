63d422bc6c2ab7637f8a07dd249ef8a6c866aa8a
Wodran Van de Sande <wodran.vandesande@dynapps.be>
2024-02-14 15:52:38 +0100

    [MIG] purchase_stock_analytic: Migration to 17.0

--- purchase_stock_analytic/models/purchase_order_line.py
+++ purchase_stock_analytic/models/purchase_order_line.py
@@ -8,7 +8,7 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     def _prepare_stock_moves(self, picking):
-        res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
+        res = super()._prepare_stock_moves(picking)
         if not self.analytic_distribution:
             return res
         for line in res:
