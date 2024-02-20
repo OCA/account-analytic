77c2b91d5df9371893308b5c51fb2281118bd866
Víctor Martínez <victor.martinez@tecnativa.com>
2024-02-20 08:19:23 +0100

    [MIG] account_analytic_sequence: Migration to 16.0
    TT47813

--- account_analytic_sequence/tests/test_analytic_sequence.py
+++ account_analytic_sequence/tests/test_analytic_sequence.py
@@ -1,19 +1,25 @@
 # Copyright 2017 ACSONE SA/NV
 # Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
 
 
 class TestAccountAnalyticSequence(TransactionCase):
-    def setUp(self):
-        super().setUp()
-
-        self.analytic_account_obj = self.env["account.analytic.account"]
-        self.partner1 = self.env.ref("base.res_partner_1")
-        self.analytic = self.analytic_account_obj.create(
-            {"name": "aa", "partner_id": self.partner1.id}
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.analytic_account_obj = cls.env["account.analytic.account"]
+        cls.partner = cls.env["res.partner"].create({"name": "Test partner"})
+        cls.plan = cls.env["account.analytic.plan"].create({"name": "Test plan"})
+        cls.analytic = cls.analytic_account_obj.create(
+            {"name": "aa", "partner_id": cls.partner.id, "plan_id": cls.plan.id}
         )
 
-    def test_onchange(self):
+    def test_account_analytic_account_code(self):
         self.assertTrue(self.analytic.code, "Sequence not added")
+        analytic2 = self.analytic_account_obj.create(
+            {"name": "Test 2", "plan_id": self.plan.id, "code": "TEST"}
+        )
+        self.assertEqual(analytic2.code, "TEST")
