6c9651a0aaa4640590834e33044862415efcdb66
Henrik Norlin <henrik.norl@gmail.com>
2024-02-25 12:02:17 +0100

    [ADD] account_move_line_analytic_account_ids & account_analytic_category (replace account.analytic.group)

---
+++ account_analytic_category/models/__init__.py
@@ -0,0 +1,4 @@
+from . import account_analytic_account
+from . import account_analytic_category
+from . import account_move
+from . import account_move_line
---
+++ account_analytic_category/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module introduces account.analytic.category
+as a replacement for account.analytic.group which was removed in Odoo 16.0.
