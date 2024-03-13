f65c8ee5e6ff041ff3dd221b4206ec0f4e91b0a2
RicardCForgeFlow <ricard.calvo@forgeflow.com>
2024-03-13 17:22:17 +0100

    [MIG] account_analytic_parent: Migration to 17.0

--- account_analytic_parent/hooks.py
+++ account_analytic_parent/hooks.py
@@ -1,10 +1,6 @@
 # Copyright 2018 Brainbean Apps
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import SUPERUSER_ID, api
 
-
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-
+def post_init_hook(env):
     env["account.analytic.account"]._parent_store_compute()
