19921737ce17bdc9f99fd62043701c7fb5d9f167
Atte Isopuro <atte.isopuro@avoin.systems>
2024-04-15 17:31:34 +0300

    [MIG] mrp_stock_analytic: Migration to 17.0
    * analytic_distribution is constrained to be valid on the MO, so we don't need to validate
      on confirmation
    * The `states`-attribute on ORM-fields is deprecated, and should be moved to views
    * mrp_account adds the distribution to the views we want, so we can just add attributes instead of re-adding the field
    * mrp.production already has mixin.analytic, no need to re-add it
    * analytic_distribution is already in the MO list view, no need to change its default visibility

--- mrp_stock_analytic/models/mrp_production.py
+++ mrp_stock_analytic/models/mrp_production.py
@@ -6,15 +6,10 @@ from odoo import fields, models
 
 
 class MrpProduction(models.Model):
-    _name = "mrp.production"
-    _inherit = ["mrp.production", "analytic.mixin"]
+    _inherit = "mrp.production"
 
     analytic_distribution = fields.Json(
         inverse="_inverse_analytic_distribution",
-        states={
-            "done": [("readonly", True)],
-            "cancel": [("readonly", True)],
-        },
     )
 
     def _inverse_analytic_distribution(self):
@@ -25,7 +20,3 @@ class MrpProduction(models.Model):
             production.move_raw_ids.write(
                 {"analytic_distribution": production.analytic_distribution}
             )
-
-    def button_mark_done(self):
-        self = self.with_context(validate_analytic=True)
-        return super().button_mark_done()
