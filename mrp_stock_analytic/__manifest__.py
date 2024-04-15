dfff8cddcfebeff6b5d49b7f5d4a5d51899fba5d
Atte Isopuro <atte.isopuro@avoin.systems>
2024-04-15 17:31:34 +0300

    [MIG] mrp_stock_analytic: Migration to 17.0
    * analytic_distribution is constrained to be valid on the MO, so we don't need to validate
      on confirmation
    * The `states`-attribute on ORM-fields is deprecated, and should be moved to views
    * mrp_account adds the distribution to the views we want, so we can just add attributes instead of re-adding the field
    * mrp.production already has mixin.analytic, no need to re-add it
    * analytic_distribution is already in the MO list view, no need to change its default visibility

--- mrp_stock_analytic/__manifest__.py
+++ mrp_stock_analytic/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Stock Analytic",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
     "category": "Manufacturing/Manufacturing",
