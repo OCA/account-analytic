816c092f954ec9370fd302ed0e451d4780d4a611
Maksym Yankin <yankinmk@gmail.com>
2024-05-30 19:28:19 +0300

    [ADD][17.0] analytic_base_department_timesheet

---
+++ analytic_base_department_timesheet/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Base Analytic Department Timesheet",
+    "summary": "Technical module to add department to timesheet analysis report",
+    "version": "17.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Projects & Services",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": ["analytic_base_department", "hr_timesheet"],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+}
