1086b42458d21832fa71155b4841cb148564d948
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2024-08-16 16:55:25 +0200

    [FIX] account_analytic_required: remove unneeded dependency
    The dependency on account_usability is not required to install the
    module, and let it do it's validation purpose.
    
    To configure it, Odoo EE users don't need anything else. In CE,
    accounting users install account_usability anyway.
    
    So this commit makes it compatible with both editions.

--- account_analytic_required/__manifest__.py
+++ account_analytic_required/__manifest__.py
@@ -3,12 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Account Analytic Required",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Analytic Accounting",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
-    "depends": ["account_usability"],
+    "depends": ["account"],
     "data": ["views/account_account_views.xml"],
     "installable": True,
 }
