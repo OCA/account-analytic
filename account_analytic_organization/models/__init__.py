031d35d6ccd8ba222fb5b9afccb8952c9e7383c0
mpascual <mpascual@apsl.net>
2024-09-18 13:47:04 +0200

    [16.0][ADD] account_analytic_organization: New module account_analytic_organization

---
+++ account_analytic_organization/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2023 APSL - Nagarro
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import res_partner
+from . import account_analytic_organization
+from . import account_move_line
+from . import account_analytic_line
---
+++ account_analytic_organization/models/account_move_line.py
@@ -0,0 +1,14 @@
+# Copyright 2023 APSL - Nagarro
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    analytic_org = fields.Char(
+        string="Analytic Organization",
+        related="partner_id.analytic_org_id.name",
+        store=True,
+    )
