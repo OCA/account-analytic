549138d861ee6d7d28e4423d95cde2abdbf3d940
Víctor Martínez <victor.martinez@tecnativa.com>
2024-09-23 13:50:33 +0200

    [IMP] account_analytic_distribution_manual: Add extra json field for export/import to analytic.mixin
    Move the functionality to the analytic.mixin model so that it can also be used in account.move.line records
    
    TT50944

--- account_analytic_distribution_manual/models/account_analytic_distribution_manual.py
+++ account_analytic_distribution_manual/models/account_analytic_distribution_manual.py
@@ -11,15 +11,6 @@ class AccountAnalyticDistributionManual(models.Model):
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
-    analytic_distribution_import = fields.Json(
-        compute="_compute_analytic_distribution_import",
-        inverse="_inverse_analytic_distribution_import",
-        readonly=False,
-        string="Analytic distribution (importable)",
-        help="Defining this field, it will set the analytical distribution in JSON "
-        "format, but using the analytic accounts names as keys of the dictionary, so it "
-        "eases the human input.",
-    )
     company_id = fields.Many2one(
         "res.company", required=True, default=lambda self: self.env.company
     )
@@ -31,34 +22,6 @@ class AccountAnalyticDistributionManual(models.Model):
             "The name must be unique per Company!",
         ),
     ]
-
-    @api.depends("analytic_distribution")
-    def _compute_analytic_distribution_import(self):
-        aa_model = self.env["account.analytic.account"]
-        for item in self:
-            data = {}
-            distribution = item.analytic_distribution
-            for key in list(distribution.keys()):
-                aa_record = aa_model.browse(int(key))
-                data[aa_record.name] = distribution[key]
-            item.analytic_distribution_import = data
-
-    def _inverse_analytic_distribution_import(self):
-        """Convert the json to the appropriate value of analytic_distribution."""
-        aa_model = self.env["account.analytic.account"]
-        for item in self:
-            base_domain = [("company_id", "in", item.company_id.ids + [False])]
-            data = {}
-            new_distribution = item.analytic_distribution_import
-            for key in list(new_distribution.keys()):
-                domain = base_domain + [("name", "=", key)]
-                aa_record = aa_model.search(
-                    domain,
-                    limit=1,
-                )
-                if aa_record:
-                    data[aa_record.id] = new_distribution[key]
-            item.analytic_distribution = data
 
     @api.returns("self", lambda value: value.id)
     def copy(self, default=None):
