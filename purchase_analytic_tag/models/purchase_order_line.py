18361c20a98315c8b6c1655d6ae9a12563723fc1
Andreu Orensanz <andreu.orensanz@forgeflow.com>
2024-11-21 16:24:23 +0100

    [MIG] purchase_analytic_tag: Migration to 17.0

--- purchase_analytic_tag/models/purchase_order_line.py
+++ purchase_analytic_tag/models/purchase_order_line.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from odoo import fields, models
+from odoo import Command, fields, models
 
 
 class PurchaseOrderLine(models.Model):
@@ -14,5 +14,5 @@ class PurchaseOrderLine(models.Model):
     def _prepare_account_move_line(self, move=False):
         vals = super()._prepare_account_move_line(move=move)
         if self.analytic_tag_ids:
-            vals.update({"analytic_tag_ids": [(6, 0, self.analytic_tag_ids.ids)]})
+            vals.update({"analytic_tag_ids": [Command.set(self.analytic_tag_ids.ids)]})
         return vals
