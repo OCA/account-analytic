76a7b96b71be199a03e63ca2ae7928cdff826723
Víctor Martínez <victor.martinez@tecnativa.com>
2025-03-04 08:59:34 +0100

    [FIX] sale_analytic_tag: Post-install test + fallback to load CoA
    Since odoo/odoo@d0342c8, the default existing company is not getting a
    CoA automatically, provoking than the current tests fail with the error:
    
    odoo.exceptions.UserError: No journal could be found in company My Company (San Francisco) for any of those types: sale
    
    Thus, we put tests post-install for being sure localization modules are
    installed, the same as AccountTestInvoicingCommon does, but we don't
    inherit from it, as it creates an overhead creating 2 new companies and
    loading their CoA and some more stuff, while we don't need all of that.
    
    Besides, if you don't have `l10n_generic_coa` installed, you can't use
    another CoA (like `l10n_es`) easily, so we put little code to select the
    first available CoA.

--- sale_analytic_tag/tests/test_sale_analytic_tag.py
+++ sale_analytic_tag/tests/test_sale_analytic_tag.py
@@ -1,15 +1,25 @@
 # Copyright 2023 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.tests import Form, common
+from odoo.tests import Form, common, tagged
 
 from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
 
 
+@tagged("-at_install", "post_install")
 class TestSaleAnalyticTag(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+        if not cls.env.company.chart_template_id:
+            # Load a CoA if there's none in current company
+            coa = cls.env.ref("l10n_generic_coa.configurable_chart_template", False)
+            if not coa:
+                # Load the first available CoA
+                coa = cls.env["account.chart.template"].search(
+                    [("visible", "=", True)], limit=1
+                )
+            coa.try_loading(company=cls.env.company, install_demo=False)
         cls.customer = cls.env["res.partner"].create({"name": "Test customer"})
         cls.product = cls.env["product.product"].create({"name": "Test product"})
         cls.plan = cls.env["account.analytic.plan"].create(
