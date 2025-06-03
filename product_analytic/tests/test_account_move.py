fccaf433753db4000836776af22a41481a3dd1cf
angel <angel.garcia@sygel.es>
2025-06-03 15:51:39 +0200

    [MIG]product_analytic: Migration to 17.0

--- product_analytic/tests/test_account_move.py
+++ product_analytic/tests/test_account_move.py
@@ -19,7 +19,7 @@ class TestAccountInvoiceLine(TransactionCase):
         cls.default_plan = cls.env["account.analytic.plan"].create(
             {
                 "name": "Default Plan",
-                "company_id": False,
+                "applicability_ids": False,
             }
         )
         cls.analytic_account1 = cls.env["account.analytic.account"].create(
