a82ab22cb541e24b5c9602c6b71b27873d734317
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-06-16 11:54:20 +0200

    [MIG] account_analytic_account_tag: Migration to 17.0

--- account_analytic_account_tag/__manifest__.py
+++ account_analytic_account_tag/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Analytic Account Tag",
     "summary": """Restore the tag_ids in account.analytic.account""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
