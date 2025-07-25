8aa1459ddc5e1c9f8753537167a2c67b2ab07a4b
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-25 13:04:35 +0200

    [FIX] sale_order_line.py

--- product_analytic_sale/models/sale_order_line.py
+++ product_analytic_sale/models/sale_order_line.py
@@ -2,13 +2,15 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import json
+
 from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    analytic_distribution = fields.Json(
+    analytic_distribution = fields.Text(
         readonly=True,
         compute="_compute_analytic_distribution",
         help="Distribution for analytic entries, as {account_id: percent}",
@@ -16,19 +18,13 @@ class SaleOrderLine(models.Model):
 
     @api.depends("product_id")
     def _compute_analytic_distribution(self):
-        """
-        Get analytic distribution from product expense analytic account
-        If no account is set, call super with those records
-        """
         for line in self:
             if line.product_id:
-                # Using product_analytic, which defines this method
                 accounts = (
                     line.product_id.product_tmpl_id._get_product_analytic_accounts()
                 )
                 income_ana = accounts.get("income")
                 if income_ana:
-                    line.analytic_distribution = {income_ana.id: 100.0}
+                    line.analytic_distribution = json.dumps({income_ana.id: 100.0})
                     continue
-            # fallback: no distribution
-            line.analytic_distribution = {}
+            line.analytic_distribution = json.dumps({})
