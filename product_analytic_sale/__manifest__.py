06ab1b9ba7cd925cc9ca9cdb1c11ce52315533de
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-28 09:26:13 +0200

    [15.0][MIG] product_analytic_sale: import module from 16.0

---
+++ product_analytic_sale/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Analytic Sale",
+    "version": "16.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Glue module between sale and product_analytic",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": ["product_analytic", "sale"],
+    "auto_install": True,
+    "installable": True,
+}
