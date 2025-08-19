39448baf3a49dd7c149a1a421f0dce845df110ba
Puriboonsiri <puri.boo@trinityroots.co.th>
2025-08-19 13:17:52 +0700

    fix: update dependency from hr to hr_timesheet

--- analytic_base_department/__manifest__.py
+++ analytic_base_department/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
     "website": "https://github.com/OCA/account-analytic",
-    "depends": ["account", "hr"],
+    "depends": ["account", "hr_timesheet"],
     "data": ["views/analytic.xml", "views/hr_department_views.xml"],
     "installable": True,
 }
