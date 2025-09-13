e230a42a7472d02fafdbe051d1698b49b63f9a7e
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-09-13 10:48:27 +0200

    [IMP][analytic_mixin_analytic_account] Compatibility with module `account_financial_report` of repo OCA/account-financial-reporting

--- analytic_mixin_analytic_account/models/analytic_mixin.py
+++ analytic_mixin_analytic_account/models/analytic_mixin.py
@@ -10,12 +10,14 @@ class AnalyticMixin(models.AbstractModel):
     analytic_account_ids = fields.Many2many(
         "account.analytic.account",
         compute="_compute_analytic_account_ids",
+        compute_sudo=True,
         context={"active_test": False},
         string="Analytic Accounts",
         help="Analytic accounts computed from analytic distribution.",
     )
     analytic_account_names = fields.Char(
         compute="_compute_analytic_account_ids",
+        compute_sudo=True,
         help="Comma-separated analytic account names, in case it is useful to be "
         "included in the exported data.",
     )
