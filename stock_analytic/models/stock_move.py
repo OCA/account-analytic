1cc8aa384977ab5de2d5706ed07f635867f8e39f
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-09-13 11:29:45 +0200

    [16.0][IMP] stock_analytic: apply distribution model

--- stock_analytic/models/stock_move.py
+++ stock_analytic/models/stock_move.py
@@ -17,6 +17,19 @@ class StockMove(models.Model):
         inverse="_inverse_analytic_distribution",
     )
 
+    @api.depends('product_id', 'partner_id', 'company_id')
+    def _compute_analytic_distribution(self):
+        """ Apply analytic distribution model """
+        for move in self:
+            distribution = self.env['account.analytic.distribution.model']._get_distribution({
+                "product_id": move.product_id.id,
+                "product_categ_id": move.product_id.categ_id.id,
+                "partner_id": move.partner_id.id,
+                "partner_category_id": move.partner_id.category_id.ids,
+                "company_id": move.company_id.id,
+            })
+            move.analytic_distribution = distribution or move.analytic_distribution
+
     def _inverse_analytic_distribution(self):
         """If analytic distribution is set on move, write it on all move lines"""
         for move in self:
