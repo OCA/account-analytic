ed3bb4bd79595fe821809dbef0def4f8c2cc8e01
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-12-23 16:01:52 +0000

    [REM] stock_analytic: Remove unrelated module from PR

--- stock_analytic/models/stock_rule.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-
-class StockRule(models.Model):
-    _inherit = "stock.rule"
-
-    def _get_custom_move_fields(self):
-        return super()._get_custom_move_fields() + ["analytic_distribution"]
--- stock_analytic/readme/DESCRIPTION.md
+++
@@ -1,2 +0,0 @@
-Adds Analytic Distribution field in stock move to be able to get
-analytic information when generating the journal items.
