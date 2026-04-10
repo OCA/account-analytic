09e52b4005fa53dda68adf265e87b6966269783d
sanami <sanami@quartile.co>
2026-04-10 09:06:01 +0000

    [MIG] analytic_mixin_analytic_account: Migration to 18.0

--- analytic_mixin_analytic_account/__manifest__.py
+++ analytic_mixin_analytic_account/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Analytic Mixin Analytic Account",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "category": "Analytic Accounting",
     "website": "https://github.com/OCA/account-analytic",
