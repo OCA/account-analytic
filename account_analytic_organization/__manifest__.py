23a0f0020f6b4b435f103d8eba7e443500cb31b9
Bhavesh Heliconia <oca@heliconia.in>
2026-05-04 14:06:10 +0530

    [MIG] account_analytic_organization: Migration to 19.0

--- account_analytic_organization/__manifest__.py
+++ account_analytic_organization/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Account Analytic Organization",
     "summary": """Adds organization field on the partner so you can use
       it on your analytic""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Analytic Accounting",
     "license": "AGPL-3",
     "author": "Miquel Pascual, Bernat Obrador, Odoo Community Association (OCA)",
