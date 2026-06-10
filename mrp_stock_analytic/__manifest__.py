02cef127ba86a60dbd565675fa60f78556e1a203
Aungkokolin1997 <lin@quartile.co>
2026-06-10 07:53:15 +0000

    [FIX] mrp_stock_analytic: add option in analytic_distribution widget

--- mrp_stock_analytic/__manifest__.py
+++ mrp_stock_analytic/__manifest__.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Stock Analytic",
+    "summary": "Adds analytic distribution in manufacturing order",
     "version": "18.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
@@ -13,5 +14,6 @@
         "views/mrp_production_views.xml",
         "views/res_config_settings_views.xml",
     ],
+    "maintainers": ["yostashiro", "aungkokolin1997"],
     "installable": True,
 }
